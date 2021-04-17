4e9b3a95c7bac442207b7606eb38eb0ada73a53f
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-17 22:16:08 +0300

    Finding the intersection between two arrays

---
+++ 3. intersectTwoArrays.py
@@ -0,0 +1,66 @@
+def mergeSort(arr):
+    if(len(arr)>1):
+        mid = len(arr)//2
+
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+
+        while(i<len(left) and j<len(right)):
+            if(left[i] < right[j]):
+                arr[k] = left[i]
+                i+=1
+                k+=1
+
+            else: 
+                arr[k] = right[j]
+                j+=1
+                k+=1
+        
+        while(i<len(left)):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while(j<len(right)):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+    return arr
+
+def intersection(lst1, lst2):
+    i = j = 0
+
+    ls1 = mergeSort(lst1) if len(lst1) < len(lst2) else mergeSort(lst2)
+    ls2 = mergeSort(lst1) if len(lst1) > len(lst2) else mergeSort(lst2)
+
+    print(ls1, ls2)
+    lst = []
+    
+    while(i<len(ls1) and j<len(ls2)):
+        if(ls1[i] == ls2[j]):
+            lst.append(ls1[i]) if ls1[i] not in ls1[:i] else lst
+            i+=1
+            j+=1
+        elif(ls1[i] < ls2[j]):
+            i+=1
+        else:
+            j+=1    
+
+    return lst
+
+lst1 = [3,1,2]
+lst2 = [1,3]
+
+arr2 = [4,4,9,5]
+arr1 = [9,4,9,8,4]
+
+arr3 = [2,6,2,9,1]
+arr4 = [7,1]
+
+print(intersection(arr3, arr4))
