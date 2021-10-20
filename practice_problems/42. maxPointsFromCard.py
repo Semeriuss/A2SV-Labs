fc40c8a7daa8b57504b35455c9139529386a6b02
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-20 23:27:01 +0300

    acceptable solution but slow

--- practice_problems/42. maxPointsFromCard.py
+++ practice_problems/42. maxPointsFromCard.py
@@ -3,22 +3,34 @@ from typing import List
 
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        possible_indices = [i - k for i in range(k + 1)] 
-        possible_indices.extend([i for i in range(1, k)])
-        n = len(possible_indices)
+        possible_elements = [cardPoints[i - k] for i in range(k + 1)] 
+        possible_elements.extend([cardPoints[i] for i in range(1, k)])
+        n = len(possible_elements)
+
+        startPointer = 0
+        endPointer = k
 
         maxPoint = 0
-        for i in range(n - k + 1):
-            maxPoint = max(maxPoint, sum([cardPoints[possible_indices[i + x]] for x in range(k)]))
+        currMax = sum([possible_elements[0 + x] for x in range(k)])
+        
+        while endPointer < n:
+            maxPoint = max(maxPoint, currMax)
+            currMax -= possible_elements[startPointer]
+            startPointer += 1
+            currMax += possible_elements[endPointer]
+            endPointer += 1
+            
+        maxPoint = max(maxPoint, currMax)
+        
         return maxPoint
 
-# ([2,2,2], 2), 
-#         ([1,2,3,4,5,6,1], 3), 
-#         ([9,7,7,9,7,7,9], 7), 
-#         ([1,1000,1], 1), 
-#         ([1,79,80,1,1,1,200,1], 3),
 
-tests = [
+
+tests = [([2,2,2], 2), 
+        ([1,2,3,4,5,6,1], 3), 
+        ([9,7,7,9,7,7,9], 7), 
+        ([1,1000,1], 1), 
+        ([1,79,80,1,1,1,200,1], 3),
         ([96,90,41,82,39,74,64,50,30], 8)]
 
 for test in tests:
