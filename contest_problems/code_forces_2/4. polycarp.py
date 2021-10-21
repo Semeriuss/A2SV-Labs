929db49b3ec6c0ba49f902553e3b17641d7fa0c7
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-21 15:16:35 +0300

    Pyrrhic victory

--- contest_problems/code_forces_2/4. polycarp.py
+++ contest_problems/code_forces_2/4. polycarp.py
@@ -9,24 +9,22 @@ for i in range(n):
     tests.append(input())
 
 def polycarp(string):
-    r = False
-    last_char = string[0]
-    count = 0 if last_char == 'v' else 1
-    for i in range(1, len(string)):
-        
-        if string[i] == 'w':
-            count += 1
-        elif string[i] == 'v' and last_char == 'v' and r == False:
-            count += 1
-            r = True
-        elif r == True:
-            r = False
-        last_char = string[i]
+    pos = 0
+    last_char = ""
+    count = 0 
     
-        
+    while pos < len(string):        
+        if string[pos] == 'w':
+            count += 1    
+        elif string[pos] == 'v' and last_char == 'v':
+            count += 1
 
+        last_char = string[pos] if last_char != 'v' else ""
+        pos += 1
+        
     return count
 
 # tests = ['vv', 'v', 'w', 'vwv', 'vwvvwv', 'vvwwvw', 'wvvvv']
+# tests = ['wvvv']
 for test in tests:
     print(polycarp(test))
