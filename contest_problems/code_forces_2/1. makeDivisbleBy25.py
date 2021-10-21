2c6e6e050e9e78978c61af88ee44ac51ab0b3426
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-21 11:59:45 +0300

    comments

--- contest_problems/code_forces_2/1. makeDivisbleBy25.py
+++ contest_problems/code_forces_2/1. makeDivisbleBy25.py
@@ -9,24 +9,24 @@ for i in range(n):
     tests.append(int(input()))
  
 def _helper(num, count):
-    print("count", count, "num", num)
+    # print("count", count, "num", num)
     if num % 25 == 0:
         return count
     if num < 25:
         return count
-    print("hherere", int(str(num)[len(str(num))-1:]))
+    # print("hherere", int(str(num)[len(str(num))-1:]))
     if int(str(num)[len(str(num))-1:]) == 0:
-        print("checkereeer", int(str(num)[len(str(num))-2:len(str(num))-1]))
+        # print("checkereeer", int(str(num)[len(str(num))-2:len(str(num))-1]))
         if int(str(num)[len(str(num))-2:len(str(num))-1]) == 0 or int(str(num)[len(str(num))-2:len(str(num))-1]) == 5:
             return count
         else:
-            print("here", num)
+            # print("here", num)
             count += 1
             num = int(str(num)[:len(str(num))-2] + str(num)[len(str(num))-1:])
-            print("after here", num)
+            # print("after here", num)
             return _helper(num, count)
     elif int(str(num)[len(str(num))-1:]) == 5:
-        print("checkereeer", int(str(num)[len(str(num))-2:len(str(num))-1]))
+        # print("checkereeer", int(str(num)[len(str(num))-2:len(str(num))-1]))
         if int(str(num)[len(str(num))-2:len(str(num))-1]) == 2 or int(str(num)[len(str(num))-2:len(str(num))-1]) == 7:
             return count
         elif int(str(num)[len(str(num))-2:len(str(num))-1]) == 0:
@@ -35,10 +35,10 @@ def _helper(num, count):
                 num = int(str(num)[:len(str(num))-1])
                 return _helper(num, count)
         else:
-            print("here", num)
+            # print("here", num)
             count += 1
             num = int(str(num)[:len(str(num))-2] + str(num)[len(str(num))-1:])
-            print("after here", num)
+            # print("after here", num)
             return _helper(num, count)
     else:
         count += 1
