eee88aa1c1cd626af983eda7f790da15ae0450e5
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-23 11:22:10 +0300

    boundary fix - still doesn't pass test case

--- contest_problems/code_forces_2/5. oneBasedArithmetic.py
+++ contest_problems/code_forces_2/5. oneBasedArithmetic.py
@@ -9,15 +9,15 @@ def _helper(num, count):
     
     n = len(str(num)) if num > 0 else len(str(num)) - 1
     if n == 1:
-        if num > 5:
+        if num > 6:
             num -= 11
             count += 2
             return _helper(num, count)
-        elif 0 < num <= 5:
+        elif 0 < num <= 6:
             num -= 1
             count += 1
             return _helper(num, count)
-        elif num < -5:
+        elif num < -6:
             num += 11
             count += 2
             return _helper(num, count)
@@ -27,20 +27,20 @@ def _helper(num, count):
             return _helper(num, count)
     else:
         if num > 0:
-            num -= int("".join(["1" for i in range(n)]))
-            count += n
+            toSubtract = int("".join(["1" for i in range(n)])) if abs(num - int("".join(["1" for i in range(n)]))) < abs(num - int("".join(["1" for i in range(n + 1)]))) else int("".join(["1" for i in range(n + 1)]))
+            num -= toSubtract
+            count += len(str(toSubtract))
             return _helper(num, count)
         else:
-            num += int("".join(["1" for i in range(n)]))
-            count += n
+            toAdd = int("".join(["1" for i in range(n)])) if abs(num - int("".join(["1" for i in range(n)]))) < abs(num - int("".join(["1" for i in range(n + 1)]))) else int("".join(["1" for i in range(n + 1)]))
+            num += toAdd
+            count += len(str(toAdd))
             return _helper(num, count)
 
 
 def oneBasedArithmetic(num):
     if num == 0:
-        return 1
-    if len(set(list(str(num)))) == 1 and str(num)[0] == '1':
-        return 0
+        return 2
     count = 0
     return _helper(num, count)
 
