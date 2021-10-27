c46b1c42fdc52e26e5fa073caab730eb8e7f8b3e
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-27 09:40:07 +0300

    leetcode contest 1

---
+++ contest_problems/leet_code_3/countValidWords.py
@@ -0,0 +1,42 @@
+class Solution:
+    def countValidWords(self, sentence: str) -> int:
+
+        def isValid(token):
+            punctuation_marks = ['!', '.', ',']
+            hyphen_count = 0
+            for i, char in enumerate(token):
+                if char.isspace():
+                    continue
+                if char.isupper():
+                    return False
+                if char.isdigit():
+                    return False
+                if char == '-':
+                    hyphen_count += 1
+                    if (i == 0 or i == len(token)-1) or hyphen_count > 1:
+                        return False
+                    if i - 1 >= 0 and not token[i-1].isalpha():
+                        return False
+                    if i + 1 <= len(token) - 1 and not token[i+1].isalpha():
+                        return False
+                if char in punctuation_marks and i != len(token)-1:
+                    return False
+            return True
+                
+                
+        tokens = sentence.split()
+        validCount = 0
+        for token in tokens:
+            if isValid(token):
+                validCount += 1
+        return validCount
+
+
+sentences = ["cat and  dog", 
+            "!this  1-s b8d!", 
+            "alice and  bob are playing stone-game10", 
+            "he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.", 
+            "a-b-c",
+            " 62   nvtk0wr4f  8 qt3r! w1ph 1l ,e0d 0n 2v 7c.  n06huu2n9 s9   ui4 nsr!d7olr  q-, vqdo!btpmtmui.bb83lf g .!v9-lg 2fyoykex uy5a 8v whvu8 .y sc5 -0n4 zo pfgju 5u 4 3x,3!wl  fv4   s  aig cf j1 a i  8m5o1  !u n!.1tz87d3 .9    n a3  .xb1p9f  b1i a j8s2 cugf l494cx1! hisceovf3 8d93 sg 4r.f1z9w   4- cb r97jo hln3s h2 o .  8dx08as7l!mcmc isa49afk i1 fk,s e !1 ln rt2vhu 4ks4zq c w  o- 6  5!.n8ten0 6mk 2k2y3e335,yj  h p3 5 -0  5g1c  tr49, ,qp9 -v p  7p4v110926wwr h x wklq u zo 16. !8  u63n0c l3 yckifu 1cgz t.i   lh w xa l,jt   hpi ng-gvtk8 9 j u9qfcd!2  kyu42v dmv.cst6i5fo rxhw4wvp2 1 okc8!  z aribcam0  cp-zp,!e x  agj-gb3 !om3934 k vnuo056h g7 t-6j! 8w8fncebuj-lq    inzqhw v39,  f e 9. 50 , ru3r  mbuab  6  wz dw79.av2xp . gbmy gc s6pi pra4fo9fwq k   j-ppy -3vpf   o k4hy3 -!..5s ,2 k5 j p38dtd   !i   b!fgj,nx qgif "]
+for sentence in  sentences:
+    print(Solution().countValidWords(sentence))
