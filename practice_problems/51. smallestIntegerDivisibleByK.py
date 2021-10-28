532b979320bfb5228e1ba010ef07f32f6512d945
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-28 17:46:06 +0300

    Better way

--- practice_problems/51. smallestIntegerDivisibleByK.py
+++ practice_problems/51. smallestIntegerDivisibleByK.py
@@ -2,12 +2,12 @@ class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
         
         if k % 2 == 0 or k % 5 == 0: return -1
-        remainders = set()
-        current_r = 1
-        while current_r not in remainders:
-            remainders.add(current_r)
-            if current_r % k == 0:
-                return len(list(str(current_r)))
-            current_r *= 10
-            current_r += 1
+
+        length = 0
+        for i in range(k):
+            length = (length * 10 + 1) % k
+            if length == 0:
+                return i + 1
         return -1
+
+print(Solution().smallestRepunitDivByK(99999))
