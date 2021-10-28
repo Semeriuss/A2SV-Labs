fe43399f437486c3f72ab8e51583a5dc2cf1b5c5
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-28 22:44:03 +0300

    better attempt

--- practice_problems/54. theWorldIsATheatre.py
+++ practice_problems/54. theWorldIsATheatre.py
@@ -1,15 +1,34 @@
 import sys
 from math import factorial
+from itertools import combinations
 
 input = sys.stdin.readline
 n, m, t = tuple(list(map(int, input().split())))
 
 def findCombination(n, m, t):
-    minBoysCombo = factorial(n)/(factorial(n-4) * factorial(4))
-    minGirlsCombo = factorial(m)/(factorial(m-1)) 
-    restCombo = (factorial(n+m-5))/factorial(t - 5) if t - 5 != 0 else 1
     
-    return int(minGirlsCombo * minBoysCombo * restCombo)
+    def getCombination(chosen_boys, chosen_girls):
+        nonlocal n, m
+        minBoysCombo = factorial(n)/(factorial(chosen_boys) * factorial(n - chosen_boys))
+        minGirlsCombo = factorial(m)/(factorial(chosen_girls) * factorial(m - chosen_girls)) 
+        return int(minGirlsCombo * minBoysCombo)
+
+    combos = []
+
+    minBoysCombo = 4
+    minGirlsCombo = t - minBoysCombo
+
+    ways = 0
+    while 0 < minGirlsCombo <= m and 3 < minBoysCombo <= n:
+        combos.append([minBoysCombo, minGirlsCombo])
+        minBoysCombo += 1
+        minGirlsCombo -= 1
+    
+    print(combos)
+    for combo in combos:
+        ways += getCombination(combo[0], combo[1])
+    
+    return ways
 
 print(findCombination(n, m, t))
 
