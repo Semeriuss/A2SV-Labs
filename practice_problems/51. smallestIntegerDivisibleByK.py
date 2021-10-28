7a8560954a73a5e4ac0fdcb8155235f16199fc59
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-28 14:10:43 +0300

    passing

--- practice_problems/51. smallestIntegerDivisibleByK.py
+++ practice_problems/51. smallestIntegerDivisibleByK.py
@@ -1,20 +1,13 @@
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
         
-        if k % 2 == 0 or k % 5 == 0:
-            return -1
-       
-        def generateOnes(n):
-            possibleNums = []
-            var = []
-            for i in range(n):
-                var.append('1')
-                possibleNums.append(int("".join(var)))
-            return possibleNums
-        
-        possibles = generateOnes(k)
-                
-        for possible in possibles:
-            if possible % k == 0:
-                return len(list(str(possible)))
+        if k % 2 == 0 or k % 5 == 0: return -1
+        remainders = set()
+        current_r = 1
+        while current_r not in remainders:
+            remainders.add(current_r)
+            if current_r % k == 0:
+                return len(list(str(current_r)))
+            current_r *= 10
+            current_r += 1
         return -1
