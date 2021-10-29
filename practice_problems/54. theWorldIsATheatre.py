c8a5acac2ee5bc5f23bfdb7b82d6d6f1490fdbdd
Semere Habtu <se.semere.habtu@gmail.com>
2021-10-29 17:20:19 +0300

    finalllyyy

--- practice_problems/54. theWorldIsATheatre.py
+++ practice_problems/54. theWorldIsATheatre.py
@@ -5,36 +5,36 @@ from itertools import combinations
 input = sys.stdin.readline
 n, m, t = tuple(list(map(int, input().split())))
 
-def findCombination(n, m, t):
+def findCombination2(n, m, t):
+    possiblities = 0
 
+    for i in range(4, t):
+        possiblities += comb(n, i) * comb(m,  t-i)
+
+    print(possiblities)
+
+def findCombination(n, m, t):
 
     def getComb(chosen_boys, chosen_girls):
         nonlocal n, m
         return comb(n, chosen_boys) * comb(m, chosen_girls)
-        
-    
-    def getCombination(chosen_boys, chosen_girls):
-        nonlocal n, m
-        minBoysCombo = factorial(n)/(factorial(chosen_boys) * factorial(n - chosen_boys))
-        minGirlsCombo = factorial(m)/(factorial(chosen_girls) * factorial(m - chosen_girls)) 
-        return int(minGirlsCombo * minBoysCombo)
 
     combos = []
 
     minBoysCombo = 4
     minGirlsCombo = t - minBoysCombo
-
+    print(minBoysCombo, minGirlsCombo)
     ways = 0
-    while 0 < minGirlsCombo <= m and 3 < minBoysCombo <= n:
+    while 0 < minGirlsCombo and 3 < minBoysCombo:
         combos.append([minBoysCombo, minGirlsCombo])
         minBoysCombo += 1
         minGirlsCombo -= 1
 
     print(combos)
     for combo in combos:
-        # ways += getCombination(combo[0], combo[1])
         ways += getComb(combo[0], combo[1])
     
     return ways
 
 print(findCombination(n, m, t))
+findCombination2(n, m, t)
