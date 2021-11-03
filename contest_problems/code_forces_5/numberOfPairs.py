f697158c2c4c77e82767ede784b050ba21108413
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-03 16:21:11 +0300

    wrong approach

--- contest_problems/code_forces_5/numberOfPairs.py
+++ contest_problems/code_forces_5/numberOfPairs.py
@@ -11,22 +11,6 @@ for i in range(t):
 def limitPairs(left, right, nums):
     possiblePair = set()
     count = 0
-    maxVal, secondMaxVal = float("-inf"), float("-inf")
-    maxInd = -1
-    for i, num in enumerate(nums):
-        if num > maxVal:
-            maxVal = num
-            maxInd = i
-    if maxInd == 0:
-        secondMaxVal = max(nums[1:])
-    elif maxInd == len(nums) - 1:
-        secondMaxVal = max(nums[:len(nums)-1])
-    else:
-        secondMaxVal = max(nums[:maxInd] + nums[maxInd+1:])
-    
-    if maxVal + secondMaxVal < left:
-        return 0
-
     for num in nums:
         if num in possiblePair:
             count += 1
@@ -41,16 +25,13 @@ def limitPairs(left, right, nums):
             possibles = right - num
             while right + possibles >= left:
                 possiblePair.add(possibles)
-                possibles -= 1   
-    print(possiblePair) 
+                possibles -= 1    
     return count
 
 
 for test in tests:
-    print()
+    
     (n,l,r), a = test
     print(limitPairs(l,r,a))
-    print()
-
 
 
