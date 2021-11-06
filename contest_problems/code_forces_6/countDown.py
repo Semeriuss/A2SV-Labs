b74c4c881ce3692c53b67e4d110c3396d1aa1fdd
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-06 09:59:55 +0300

    memoized - accepted!

--- contest_problems/code_forces_6/countDown.py
+++ contest_problems/code_forces_6/countDown.py
@@ -9,26 +9,33 @@ for i in range(testcases):
     digits = input().rstrip()
     tests.append((n, digits))
 
-def countDown(digits, n):
+def countDown(digits, n, memo):
     if n < 0:
         return 0
 
+    if n in memo:
+        return memo[n]
+
     if n >= len(digits):
-        return countDown(digits, n - 1)
+        memo[n] = countDown(digits, n - 1, memo)
+        return memo[n]
 
     if int(digits[n]) == 0:
-        return countDown(digits, n - 1)
+        memo[n] = countDown(digits, n - 1, memo)
+        return memo[n]
     
     elif int(digits[n]) != 0 and n == len(digits) - 1:
-        return int(digits[n]) + countDown(digits, n - 1)
-        
+        memo[n] = int(digits[n]) + countDown(digits, n - 1, memo)
+        return memo[n]
+
     else:
-        return 1 + int(digits[n]) + countDown(digits, n - 1)
+        memo[n] = 1 + int(digits[n]) + countDown(digits, n - 1, memo)
+        return memo[n]
         
 
 
 for test in tests:
     n, digits = test
-    print(countDown(digits, n))
+    print(countDown(digits, n, {}))
         
 
