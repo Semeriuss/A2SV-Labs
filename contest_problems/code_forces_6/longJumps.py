1cb9853a6654907f6764d6c94e9f0b1fb73da759
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-06 18:36:52 +0300

    bottom up approach

--- contest_problems/code_forces_6/longJumps.py
+++ contest_problems/code_forces_6/longJumps.py
@@ -1,41 +1,21 @@
-import sys
 
-sys.setrecursionlimit(300000)
 testcases = int(input())
 
 tests = []
+
 for i in range(testcases):
-    jumps2 = [0]
     n = int(input())
     jumps = list(map(int, input().split()))
-    jumps2.extend(jumps)
-    tests.append(jumps2)
-
-def calculateJump(start, jumps, score, memo):
-
-    if start >= len(jumps):
-        return 0
-
-    if memo[start]:
-        return memo[start]
-
-    
-    memo[start] = jumps[start] + calculateJump(start + jumps[start], jumps, score, memo)
-    return memo[start]
-
-
+    tests.append(jumps)
 
 def findLongestJump(jumps): 
-    maxJump = 0 
-    memo = [0 for _ in range(len(jumps) + 1)] 
-    
-    for start in range(1, len(jumps)): 
-        if memo[start] != 0:
-            maxJump = max(maxJump, memo[start])
+    memo = {}
+    for start in range(len(jumps) - 1, -1, -1): 
+        if start + 1 + jumps[start] in memo:
+            memo[start + 1] = memo[start + 1 + jumps[start]] + jumps[start]
         else:
-            maxJump = max(maxJump, calculateJump(start, jumps, 0, memo))
-
-    return maxJump
+            memo[start + 1] = jumps[start]
+    return max(memo.values())
 
 for test in tests:
     print(findLongestJump(test))
