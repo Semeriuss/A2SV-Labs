ac41821542f0df66295d8c8876280d75826f65b9
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-07 10:32:02 +0300

    passed

--- contest_problems/code_forces_6/68. wordCountEngine.py
+++ contest_problems/code_forces_6/68. wordCountEngine.py
@@ -8,9 +8,13 @@ def word_count_engine(document):
     words = [word.lower() for word in docString.split()]
 
 #   wordMap = Counter(words)
-    return sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
+    # return sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
 #   return [list(t) for t in wordMap.most_common()]
 #   return [[word, str(count)] for word, count in wordMap.most_common()]
+    result = sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
   
+    for i in range(len(result)):
+        result[i][1] = str(result[i][1])
+    return result
 input_doc = "Practice makes perfect, you'll get perfecT by practice. just practice! just just just!!"
 print(word_count_engine(input_doc))
