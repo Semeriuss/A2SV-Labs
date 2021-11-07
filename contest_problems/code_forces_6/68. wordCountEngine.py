3eee82e6f0365a566a3306b3b94fd846eb88d469
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-07 10:22:32 +0300

    other methods

--- contest_problems/code_forces_6/68. wordCountEngine.py
+++ contest_problems/code_forces_6/68. wordCountEngine.py
@@ -3,12 +3,14 @@ import string
 
 
 def word_count_engine(document):
-  docString = document.translate(str.maketrans('', '', string.punctuation))
-  words = [word.lower() for word in docString.split()]
 
-  wordMap = Counter(words)
-  
-  return [[word, str(count)] for word, count in wordMap.most_common()]
+    docString = document.translate(str.maketrans('', '', string.punctuation))
+    words = [word.lower() for word in docString.split()]
+
+#   wordMap = Counter(words)
+    return sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
+#   return [list(t) for t in wordMap.most_common()]
+#   return [[word, str(count)] for word, count in wordMap.most_common()]
   
 input_doc = "Practice makes perfect, you'll get perfecT by practice. just practice! just just just!!"
 print(word_count_engine(input_doc))
