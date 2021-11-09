5421def5400821a7fe839dcc011c94b4ae76e98d
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-09 09:22:07 +0300

    fixed start pointer range and boundary

--- contest_problems/code_forces_7/ternaryString.py
+++ contest_problems/code_forces_7/ternaryString.py
@@ -5,33 +5,67 @@ for i in range(t):
     tests.append([int(char) for char in input()])
 
 def ternaryString(nums):
-    if len(set(nums)) != 3: 
-        return 0
-    
+    nummap = {1: 0, 2: 0, 3: 0}
+
+    ep = 0
     sp = 0
-    ep = 1
-    numset = set([1, 2, 3])
 
-
-    # numset.remove(nums[sp])
-    while numset and ep < len(nums):
-        if nums[sp] == nums[ep]:
-            sp = ep
-            
-
-        if nums[ep] in numset:
-            numset.remove(nums[ep])
-            if not numset:
-                break
-        print(sp, ep, nums, numset)
-        ep += 1
-
-        
+    def allIn(nummap):
+        return nummap[1] != 0 and nummap[2] != 0 and nummap[3] != 0
     
-    
-    return ep - sp
-            
-
+    minLength = float("inf") 
+    nummap[nums[0]] += 1
+    while sp < len(nums) - 1 and ep < len(nums):
+        if allIn(nummap):
+            minLength = min(minLength, ep - sp + 1)
+            nummap[nums[sp]] -= 1
+            sp += 1
         
+        else:
+            ep += 1
+            if ep < len(nums):
+                nummap[nums[ep]] += 1
+    return minLength if minLength != float("inf") else 0
+ 
+
+res = []
 for test in tests:
-    print(ternaryString(test))
+    res.append(ternaryString(test))
+
+print(*res, sep="\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3
+# 1
+# 3
+# 2
+# 3
+# 3...
+
+
+
+
+# 3
+# 3
+# 3
+# 3
+# 3
+# 3...
