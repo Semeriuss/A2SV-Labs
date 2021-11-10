acbccd131d178e2763fd23dd978f3c5a8d583b5c
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-10 13:44:28 +0300

    implemented union find

---
+++ basic_algorithms/union_find.py
@@ -0,0 +1,20 @@
+
+n = 8
+root = [i for i in range(n + 1)]
+size = [1 for i in range(n + 1)]
+
+def find(node):
+    if node != root[node]:
+        root[node] = find(root[node])
+    return root[node]
+
+def union(node1, node2):
+    smallRoot, bigRoot = sorted([find(node1), find(node2)], key=lambda x: size[x])
+
+    if smallRoot != bigRoot:
+        root[smallRoot] = bigRoot
+        size[bigRoot] += size[smallRoot]
+    
+union(3, 4)
+print(find(3))
+    
