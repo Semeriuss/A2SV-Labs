48f4902b2ae38cfbe421c853db202b15eddac73b
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-11 14:21:01 +0300

    longest L subsequence

--- contest_problems/code_forces_9/frogJump.py
+++ contest_problems/code_forces_9/frogJump.py
@@ -33,16 +33,29 @@ def frogJump(s):
     
     return longestJump
 
+def frogJump(s):
+    
+    sp = 0
+    ep = 0
+
+    longestJump = 0
+    currJump = 0
+    while ep < len(s):
+        if s[ep] == 'L':
+            currJump += 1
+        else:
+            sp = ep
+            currJump = 0
+        ep += 1
+        longestJump = max(longestJump, currJump)
+    
+    return longestJump + 1
 
 
 
-# s = "LRLRRLL"
-# s = "L"
-# s = "LLR"
-# s = "RRRR"
-# s = "LLLLLL"
-# s = "R"
-# print(frogJump(s))
+# tests = ["LRLRRLL", "L", "LLR", "RRRR", "LLLLLL", "R"]
+# for test in tests:
+#     print(frogJump(test))
 
 if __name__ == '__main__':
     t = int(input())
