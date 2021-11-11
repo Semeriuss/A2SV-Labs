71934c1ea3fe3b42addafea62ea77d003dd5912b
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-11 09:39:47 +0300

    nlogn using binary search

--- practice_problems/18. findDuplicate.py
+++ practice_problems/18. findDuplicate.py
@@ -31,7 +31,7 @@ def findDuplicate(nums):
         while True:
             tortoise = nums[tortoise]
             hare = nums[nums[hare]]
-            if tortoise == hair:
+            if tortoise == hare:
                 break
 
         tortoise = nums[0]
@@ -40,3 +40,40 @@ def findDuplicate(nums):
             hare = nums[hare]
 
         return hare
+    
+def findDuplicate(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def numbersLessThanOrEqualTo(n, nums):
+            count = 0
+            for num in nums:
+                if num <= n:
+                    count += 1
+            return count
+        
+        lower = 1
+        upper = len(nums) - 1
+        ans = -1
+        while lower <= upper:
+            mid = (lower + upper)//2
+            count = numbersLessThanOrEqualTo(mid, nums)
+            # print("mid:", mid, "count:", count)  
+            if count <= mid:
+                lower = mid + 1
+            elif count > mid:
+                ans = mid
+                upper = mid - 1
+        
+        return ans
+
+nums = [1, 2, 3, 7, 5, 6, 8, 8]
+# nums = [1, 2, 3, 7, 5, 6, 7, 7]
+# nums = [1, 2, 2, 3, 4, 6, 7, 8, 9]
+# nums = [1, 3, 4, 2, 2]
+# nums = [1,2,3,3,4]
+print(findDuplicate(nums))
+
+            
+            
