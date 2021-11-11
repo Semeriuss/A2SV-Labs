139e76ca1f62f1dafb8dc24d023d560d92bde32c
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-11 15:51:21 +0300

    read the problem - doesn't need n == 1 case

--- contest_problems/code_forces_9/cormen.py
+++ contest_problems/code_forces_9/cormen.py
@@ -1,48 +1,14 @@
-
-def cormen(nums, k):
-
-    totalNeededWalks = 0
-    if len(nums) == 1:
-        additionalWalks = [nums[0] + k - nums[0]]
-        totalNeededWalks = k - nums[0]
-        # break here
-    else:
-        additionalWalks = [nums[0]]
-        for i in range(1, len(nums)):
-            totalNeededWalks += (k - additionalWalks[i - 1] - nums[i])
-            additionalWalks.append((k - additionalWalks[i - 1]))
-
-
-    print(totalNeededWalks)
-    print(additionalWalks)
-
 def cormen(nums, k):
     
     totalNeededWalks = 0
-    additionalWalks = nums.copy()
-    needsMoreWalks = False
-    if len(nums) == 1 and nums[0] < k:
-        needsMoreWalks = True
-        
+    additionalWalks = [nums[0]]
+    
     for i in range(1, len(nums)):
-        walks = nums[i - 1] + nums[i]
-        if walks < k:
-            needsMoreWalks = True
-            break
-    
-    if needsMoreWalks:
-        if len(nums) == 1:
-            additionalWalks = [nums[0] + k - nums[0]]
-            totalNeededWalks = k - nums[0]
-        
+        if additionalWalks[i - 1] + nums[i] < k:
+            additionalWalks.append(nums[i] + k - additionalWalks[i - 1] - nums[i])
+            totalNeededWalks += (k - additionalWalks[i - 1] - nums[i])
         else:
-            additionalWalks = [nums[0]]
-            for i in range(1, len(nums)):
-                totalNeededWalks += (k - additionalWalks[i - 1] - nums[i])
-                additionalWalks.append((k - additionalWalks[i - 1] - nums[i]))
-
-            for j in range(1, len(nums)):
-                additionalWalks[j] += nums[j]
+            additionalWalks.append(nums[i])
     
             
     print(totalNeededWalks)
