1b34701bd8299e49df708ea3325cded176a459b3
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-12 16:50:57 +0300

    clean up

--- contest_problems/code_forces_9/frogJump.py
+++ contest_problems/code_forces_9/frogJump.py
@@ -1,38 +1,3 @@
-
-def frogJump(s):
-    
-    firstR = -1
-    lastR = -1
-
-    for i in range(len(s)):
-        if s[i] == 'R':
-            firstR = i
-            break
-    
-    for j in range(len(s) - 1, -1, -1):
-        if s[j] == 'R':
-            lastR = j + 1
-            break
-    
-    # print(firstR, lastR)
-    if firstR < 0 and lastR < 0:
-        return len(s) + 1
-    
-    else:
-        return max(firstR, lastR) + 1
-
-def frogJump(s):
-    
-    longestJump = 0
-    for i in s:
-        if i == 'L':
-            longestJump += 1
-        else:
-            longestJump = 0
-        longestJump = max(longestJump, 1)
-    
-    return longestJump
-
 def frogJump(s):
     
     sp = 0
@@ -50,12 +15,6 @@ def frogJump(s):
         longestJump = max(longestJump, currJump)
     
     return longestJump + 1
-
-
-
-# tests = ["LRLRRLL", "L", "LLR", "RRRR", "LLLLLL", "R"]
-# for test in tests:
-#     print(frogJump(test))
 
 if __name__ == '__main__':
     t = int(input())
