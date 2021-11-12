eb269743c5c01515e71cf9d5b65ba0918f7b2478
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-12 10:16:53 +0300

    runtime error

--- contest_problems/code_forces_5/mochaAndDiana.py
+++ contest_problems/code_forces_5/mochaAndDiana.py
@@ -24,16 +24,12 @@ def findMaxConnectableEdges(n, mochaEdges, dianaEdges):
         union(node1, node2, dianaForest, dianaSize)
         
     connectableEdges = []
-    for node1 in range(1, n+1):
-        mochaRoot = find(node1, mochaForest)
-        for node2 in range(1, n+1):
-            dianaRoot = find(node2, dianaForest)
-            if connected(mochaRoot, dianaRoot, mochaForest) or connected(mochaRoot, dianaRoot, dianaForest):
-                continue
-            if mochaRoot != dianaRoot:
+    for node1 in range(1, n + 1):
+        for node2 in range(i + 1, n + 1):
+            if not connected(node1, node2, mochaForest) and not connected(node1, node2, dianaForest):
                 connectableEdges.append(f'{node1} {node2}')
-                union(mochaRoot, dianaRoot, mochaForest, mochaSize)
-                union(mochaRoot, dianaRoot, dianaForest, dianaSize)
+                union(node1, node2, mochaForest, mochaSize)
+                union(node1, node2, dianaForest, dianaSize)
 
     maxConnectableEdges = len(connectableEdges)
     print(maxConnectableEdges)
@@ -44,10 +40,10 @@ if __name__ == "__main__":
     n, m1, m2 = list(map(int, input().split()))
     mochaEdges = []
     for i in range(m1):
-        mochaEdges.append(tuple(list(map(int, input().split()))))
+        mochaEdges.append(list(map(int, input().split())))
 
     dianaEdges = []
     for j in range(m2):
-        dianaEdges.append(tuple(list(map(int, input().split()))))
+        dianaEdges.append(list(map(int, input().split())))
     
     findMaxConnectableEdges(n, mochaEdges, dianaEdges)
