7d4def03581ddfc36293fc8429d10ded1af63fc3
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-12 13:57:29 +0300

    more efficient

--- contest_problems/code_forces_9/zeroRemainder.py
+++ contest_problems/code_forces_9/zeroRemainder.py
@@ -1,35 +1,36 @@
+from collections import Counter
+
 def zeroRemainderArray(nums, k):
-    
-    numbersToModify = 0
-    for i in range(len(nums)):
-        if nums[i] % k != 0:
-            numbersToModify += 1
-    
-    if numbersToModify == 0:
-        return 0
-    
-    def foundDivisibleNumber(nums,x, k):
-        found = False
-        changedNum = 0
-        for i in range(len(nums)):
-            if nums[i] % k == 0:
-                continue
-            if (nums[i] + x) % k == 0:
-                changedNum = nums[i] + x
-                nums[i] += x
-                found = True
-                break
-            
-        if found: nums.remove(changedNum)
-        return found
+    requiredAdditions = []
 
     x = 1
-    while numbersToModify > 0:
-        if foundDivisibleNumber(nums, x, k):
-            numbersToModify -= 1
-        x += 1
+    for num in nums:
+        if num % k != 0:
+            requiredAdditions.append(k - num%k)
+        else:
+            requiredAdditions.append(0)
     
-    return x
+    additionsMap = Counter(requiredAdditions)
+    sortedSteps = sorted(additionsMap.items(), key=lambda x: (x[1], x[0]), reverse=True)
+
+    maxSteps, maxAmount = 0, 0
+    for amount, steps in sortedSteps:
+        if amount != 0 and steps != 0:
+            maxSteps = steps
+            maxAmount = amount
+            break
+        
+    # print(sortedSteps, maxSteps, maxAmount)
+    if maxSteps == 0 or maxAmount == 0:
+        return 0
+    else:
+        requiredSteps = 0
+        for i in range(maxSteps):
+            requiredSteps = max(((k*i) + maxAmount), requiredSteps)
+        return requiredSteps + 1
+
+    
+        
 
 # tests = [([1, 2, 1, 3], 3), 
 #         ([8, 7, 1, 8, 3, 7, 5, 10, 8, 9], 6), 
@@ -49,5 +50,4 @@ if __name__ == "__main__":
         nums = list(map(int, input().split()))
         res.append(zeroRemainderArray(nums, k))
     print(*res, sep="\n")
-        
         
