945af4dc3fdb4c95f48f9b1c02521ad2ba5d30ee
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-12 16:06:17 +0300

    two pointer - O(n) solution

--- contest_problems/code_forces_9/pairOfTopics.py
+++ contest_problems/code_forces_9/pairOfTopics.py
@@ -6,38 +6,20 @@ def pairOfTopics(teacherInterest, studentInterest):
         goodness[i] = teacherInterest[i] - studentInterest[i]
 
     goodness.sort()
-
-    def countFittingPair(target, goodness):
-        lower = 0
-        upper = len(goodness)
-
-        best = -1
-        while lower <= upper:
-            mid = (lower + upper)//2
-            if goodness[mid] < target:
-                lower = mid + 1
-            
-            elif goodness[mid] > target:
-                best = mid
-                upper = mid - 1
-            
-            else:
-                lower = mid + 1
-        
-        return best
-
+    sp = 0
+    ep = n - 1
     goodPairs = 0
-    for i in range(len(goodness)):
-        target = -1 * goodness[i]
-        goodIndex = countFittingPair(target, goodness[i:]) 
-        res = len(goodness[goodIndex:])
-        print(goodness[i],"good", target, goodIndex, res)
-        goodPairs += res if res > 0 else 0
+    while sp < ep:
+        if goodness[ep] + goodness[sp] > 0:
+            goodPairs += ep - sp
+            ep -= 1
+        else:
+            sp += 1
+
     return goodPairs
-
-ti = [4, 8, 2, 6, 2]
-si = [4, 5, 4, 1, 3]
-
-res = [-2, -1, 0,  3, 5]
-print(pairOfTopics(ti, si))
     
+if __name__ == "__main__":
+    n = int(input())
+    teacherInterest = list(map(int, input().split()))
+    studentInterest = list(map(int, input().split()))
+    print(pairOfTopics(teacherInterest, studentInterest))
