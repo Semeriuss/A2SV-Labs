d7963259b51be49def4a4872609ad601a0ecbacd
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-13 12:28:48 +0300

    wrong because of 1 1 I think

--- contest_problems/code_forces_10/joysticks.py
+++ contest_problems/code_forces_10/joysticks.py
@@ -1,4 +1,3 @@
-
 def joysticks(a, b):
     if a <= 0 or b <= 0:
         return 0
@@ -6,7 +5,6 @@ def joysticks(a, b):
     elif a == b == 1:
         return 0
     else:
-        # return 1 + max(joysticks(a + 1, b - 2), joysticks(a - 2, b + 1))
         if a < b:
             return 1 + joysticks(a + 1, b - 2)
         
@@ -18,14 +16,3 @@ def joysticks(a, b):
 if __name__ == "__main__":
     a, b = list(map(int, input().split()))
     print(joysticks(a, b))
-
-# def joysticks(a, b):
-#     if a <= 0 or b <= 0:
-#         return 0
-    
-#     else:
-#         return 1 + max(joysticks(a + 1, b - 2), joysticks(a - 2, b + 1))
-
-# if __name__ == "__main__":
-#     a, b = list(map(int, input().split()))
-#     print(joysticks(a, b))
