ccf4a2f71017ac3d70962b6391c7a2dd9a833d14
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-13 12:31:38 +0300

    yess! 1 1 was the edge case

--- contest_problems/code_forces_10/joysticks.py
+++ contest_problems/code_forces_10/joysticks.py
@@ -4,14 +4,13 @@ def joysticks(a, b):
     
     elif a == b == 1:
         return 0
+        
     else:
         if a < b:
             return 1 + joysticks(a + 1, b - 2)
         
         else:
             return 1 + joysticks(a - 2, b + 1)
-
-
 
 if __name__ == "__main__":
     a, b = list(map(int, input().split()))
