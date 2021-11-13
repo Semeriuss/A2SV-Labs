ee3462415e963a3612ee6479f60f491f1fc23b1e
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-13 12:19:26 +0300

    managed edge case [-1, 3, 0]

--- contest_problems/code_forces_10/makeProductEqualOne.py
+++ contest_problems/code_forces_10/makeProductEqualOne.py
@@ -4,25 +4,37 @@ def makeProductEqualOne(nums):
 
     evenNegativesCase = 0
     oddNegativesCase = []
+    zeroCount = 0
 
     negativeCount = 0
 
     cost = 0
     for num in nums:
-        if num >= 0:
+        if num > 0:
             cost += abs(num - 1)
-        else:
+        elif num < 0:
             negativeCount += 1
             evenNegativesCase += abs(num + 1)
             heapq.heappush(oddNegativesCase, num)
+        else:
+            zeroCount += 1
 
     if negativeCount % 2 == 0:
+        cost += zeroCount
         return cost + evenNegativesCase
 
     while len(oddNegativesCase) > 1:
         cost += abs(heapq.heappop(oddNegativesCase) + 1)
     
-    cost += abs(heapq.heappop(oddNegativesCase) - 1)
+    finalNegative = heapq.heappop(oddNegativesCase)
+    if zeroCount > 0:
+        cost += 1
+        cost += abs(finalNegative + 1)
+        zeroCount -= 1
+    else:
+        cost += abs(finalNegative - 1)
+
+    cost += zeroCount
 
     return cost
 
@@ -30,10 +42,10 @@ def makeProductEqualOne(nums):
 # for test in tests:
 #     print(makeProductEqualOne(test))
 
-test = []
-print(makeProductEqualOne(test))
+# test = [-1, 3, 0]
+# print(makeProductEqualOne(test))
 
-# if __name__ == "__main__":
-#     n = int(input())
-#     nums = list(map(int, input().split()))
-#     print(makeProductEqualOne(nums))
+if __name__ == "__main__":
+    n = int(input())
+    nums = list(map(int, input().split()))
+    print(makeProductEqualOne(nums))
