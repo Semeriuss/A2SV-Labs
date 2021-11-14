c8e8e5a586c298b96c2f0bf8156f4b8dd91475f0
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-14 19:05:57 +0300

    reverse greedy way - correct way

--- contest_problems/code_forces_10/minimalLabels.py
+++ contest_problems/code_forces_10/minimalLabels.py
@@ -1,35 +1,35 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 import heapq
 
-def minimalLabels(graph, indegree):
+def minimalLabels(graph, outdegree):
 
     queue = []
-    n = len(indegree)
+    n = len(outdegree)
 
     label = [0] * (n)   
     for node in range(1, n):
-        if indegree[node] == 0:
-            heapq.heappush(queue, node)
+        if outdegree[node] == 0:
+            heapq.heappush(queue, -node)
 
-    rank = 1
+    rank = n - 1
     while queue:
         current = heapq.heappop(queue)
-        label[current] = str(rank)
-        rank += 1
-        for neighbor in graph[current]:
-            indegree[neighbor] -= 1
-            if indegree[neighbor] == 0:
-                heapq.heappush(queue, neighbor)
+        label[abs(current)] = str(rank)
+        rank -= 1
+        for parent in graph[current]:
+            outdegree[abs(parent)] -= 1
+            if outdegree[abs(parent)] == 0:
+                heapq.heappush(queue, parent)
 
     return " ".join(label[1:])       
 
 if __name__ == "__main__":
     n, m = list(map(int, input().split()))
-    indegree = [0] * (n + 1)
+    outdegree = [0] * (n + 1)
     graph = defaultdict(list)
     for _ in range(m):
         edge1, edge2 = map(int, input().split())
-        graph[edge1].append(edge2)
-        indegree[edge2] += 1
-    print(minimalLabels(graph, indegree))
+        graph[-edge2].append(-edge1)
+        outdegree[abs(edge1)] += 1
+    print(minimalLabels(graph, outdegree))
 
