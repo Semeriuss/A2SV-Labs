da226ebf2ab8d2b679e896c61415ce4329efd36f
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-16 11:03:54 +0300

    tle - 50 x 50 [G]

--- contest_problems/code_forces_11/solveMaze.py
+++ contest_problems/code_forces_11/solveMaze.py
@@ -1,6 +1,3 @@
-from collections import deque
-# import sys
-# sys.setrecursionlimit(100000)
 def solveMaze(maze):
     ROWSIZE = len(maze)
     COLSIZE = len(maze[0])
@@ -9,27 +6,30 @@ def solveMaze(maze):
         return 0 <= n < ROWSIZE and 0 <= m < COLSIZE
 
     directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
-    badMeetsGood = False
+
     def closePath(graph, source):
         cRow, cCol = source
         for direction in directions:
             sideRow, sideCol = cRow + direction[0], cCol + direction[1]
-            if fitsBoundary(sideRow, sideCol):
+            if fitsBoundary(sideRow, sideCol):   
                 cCell = graph[sideRow][sideCol]
                 if (cCell == "."):
                     graph[sideRow][sideCol] = "#"
-                elif cCell == "G":
-                    badMeetsGood == True
-                    # closePath(graph, (sideRow, sideCol))
+                elif cCell == "G": 
+                    return False
+        return True
 
-    def hasPath(graph, src, dst):
+    def hasPath(graph, src, dst, visited):
         if src == dst: return True
         sRow, sCol = src
-        for neighbor in directions:
-            nRow, nCol = sRow + neighbor[0], sCol + neighbor[1]
+        visited.add(src)
+        for x, y in directions:
+            nRow, nCol = sRow + x, sCol + y
             if fitsBoundary(nRow, nCol) and \
                 graph[nRow][nCol] != "#" and \
-                hasPath(graph, (nRow, nCol), dst): return True
+                graph[nRow][nCol] != "B" and \
+                (nRow, nCol) not in visited and \
+                hasPath(graph, (nRow, nCol), dst, visited): return True
         return False 
 
     target = (ROWSIZE - 1, COLSIZE - 1)
@@ -38,19 +38,18 @@ def solveMaze(maze):
         for j in range(COLSIZE):
             cCell = maze[i][j]
             if cCell == "B":
-                closePath(maze, (i, j))
+                if not closePath(maze, (i, j)):
+                    return "No"
             elif cCell == "G":
                 goods.append((i, j))
     
-    if badMeetsGood:
-        return "No"
 
     if maze[ROWSIZE - 1][COLSIZE - 1] != "#" and len(goods) == 0:
         return "YES"
-    print(maze)
+
     pathExists = True
     for good in goods:
-        if not hasPath(maze, good, target):
+        if not hasPath(maze, good, target, set()):
             pathExists = False
             break
     return "YES" if pathExists else "NO"    
@@ -65,5 +64,4 @@ if __name__ == "__main__":
             maze.append(list(input()))
         res.append(solveMaze(maze))
     print(*res, sep="\n")
-
 
