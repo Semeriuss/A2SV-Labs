b86a44744cd68e27487839af62a0283f3673c676
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-17 09:41:44 +0300

    wrong for 00110

--- contest_problems/code_forces_11/armChairs.py
+++ contest_problems/code_forces_11/armChairs.py
@@ -1,33 +1,17 @@
 def armChairs(seats):
-    ones, zeros = [], []
+    stack = [[]]
     minutes = 0
-
     for i, seat in enumerate(seats):
-        if seat == 1:
-            ones.append(i)
+        if stack[-1] == []:
+            stack.append([seat, i])
         else:
-            zeros.append(i)
-    
-    for i in range(len(ones)):
-        minutes += abs(ones[i] - zeros[i])
-    
+            if stack[-1][0] != seat:
+                val = stack.pop()
+                minutes += (i - val[1])
+            else:
+                stack.append([seat, i])
     return minutes
 
-def armChairs(seats):
-    sp, ep = 0, 0
-    minutes = 0
-    
-    while ep < len(seats):
-        if seats[sp] == seats[ep]:
-            ep += 1
-        elif seats[sp] != seats[ep]:
-            minutes += ep - sp 
-            sp +=  1
-            ep +=  1
-        
-        
-    
-    return minutes
 if __name__ == "__main__":
     n = int(input())
     seats = list(map(int, input().split()))
