f31a49f373cbc10f18d71eae097827ea59d4f22e
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-17 09:12:46 +0300

    preliminary attempts

--- contest_problems/code_forces_11/armChairs.py
+++ contest_problems/code_forces_11/armChairs.py
@@ -1,18 +1,33 @@
 def armChairs(seats):
-    sp, ep = 0, 0
+    ones, zeros = [], []
     minutes = 0
-    while ep < len(seats):
-        if seats[sp] == seats[ep] == 1:
-            ep += 1
-        elif seats[sp] == seats[ep] == 0:
-            ep += 1
-            sp += 1
+
+    for i, seat in enumerate(seats):
+        if seat == 1:
+            ones.append(i)
         else:
-            minutes += (ep - sp)
-            sp += 1
-            ep += 1
+            zeros.append(i)
+    
+    for i in range(len(ones)):
+        minutes += abs(ones[i] - zeros[i])
+    
     return minutes
 
+def armChairs(seats):
+    sp, ep = 0, 0
+    minutes = 0
+    
+    while ep < len(seats):
+        if seats[sp] == seats[ep]:
+            ep += 1
+        elif seats[sp] != seats[ep]:
+            minutes += ep - sp 
+            sp +=  1
+            ep +=  1
+        
+        
+    
+    return minutes
 if __name__ == "__main__":
     n = int(input())
     seats = list(map(int, input().split()))
