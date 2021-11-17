fa78730205ff68b2611450817a91ab1f325f30eb
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-17 11:30:36 +0300

    strings

---
+++ practice_problems/68. wordCountEngine.py
@@ -0,0 +1,20 @@
+from collections import defaultdict, Counter
+import string
+
+
+def word_count_engine(document):
+
+    docString = document.translate(str.maketrans('', '', string.punctuation))
+    words = [word.lower() for word in docString.split()]
+
+#   wordMap = Counter(words)
+    # return sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
+#   return [list(t) for t in wordMap.most_common()]
+#   return [[word, str(count)] for word, count in wordMap.most_common()]
+    result = sorted([list(t) for t in Counter(words).items()], key=lambda x: (-x[1], words.index(x[0])))
+  
+    for i in range(len(result)):
+        result[i][1] = str(result[i][1])
+    return result
+input_doc = "Practice makes perfect, you'll get perfecT by practice. just practice! just just just!!"
+print(word_count_engine(input_doc))
