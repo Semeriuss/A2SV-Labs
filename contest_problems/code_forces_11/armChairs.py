24ceebd3cd831de9a5a67172b22ddf58a6e37bb3
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-17 10:30:46 +0300

    graph?

--- contest_problems/code_forces_11/armChairs.py
+++ contest_problems/code_forces_11/armChairs.py
@@ -1,18 +1,38 @@
+from collections import deque
+
 def armChairs(seats):
-    stack = [[]]
-    minutes = 0
+    ones, zeros = [], []
     for i, seat in enumerate(seats):
-        if stack[-1] == []:
-            stack.append([seat, i])
+        if seat == 1:
+            ones.append(i)
         else:
-            if stack[-1][0] != seat:
-                val = stack.pop()
-                minutes += (i - val[1])
-            else:
-                stack.append([seat, i])
+            zeros.append(i)
+    
+    def dfs(graph, source, visited):
+        if source >= len(graph) or source < 0:
+            return float("inf")
+        if source not in visited and graph[source] == 0:
+            visited.add(source)
+            return source
+        right = dfs(graph, source + 1, visited)
+        left = dfs(graph, source - 1, visited)
+        if right < left:
+            visited.remove(left)
+            return right
+        else:
+            visited.remove(right)
+            return left
+    
+    minutes = 0
+    visited = set()
+    for one in ones:
+        minutes += abs(one - dfs(seats, one, visited))
+    
     return minutes
+
 
 if __name__ == "__main__":
     n = int(input())
     seats = list(map(int, input().split()))
     print(armChairs(seats))
+    
