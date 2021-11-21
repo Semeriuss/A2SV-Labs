9fd84411c3196ed1ea2de53a13b48abccd7acb85
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-21 13:07:26 +0300

    hashmap solution

--- practice_problems/77. longestSubstringWithoutRepeatingChars.py
+++ practice_problems/77. longestSubstringWithoutRepeatingChars.py
@@ -1,18 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        visited = set()
+        visited = {}
         sp, ep = 0, 0
         count = 0
-        print(len(s))
         while ep < len(s):
-            if s[ep] in visited:
-                count = max(count, ep - sp)
-                visited.remove(s[sp])
-                sp += 1
+            print(sp, ep, count, s[sp], s[ep], s)
+            print(visited)
+            if s[ep] in visited and sp <= visited[s[ep]]:
+                sp = visited[s[ep]]
             else:
-                visited.add(s[ep])
-                ep += 1
+                count = max(count, ep - sp)
+            visited[s[ep]] = ep
+            ep += 1
         count = max(count, ep - sp)
         return count
     
-print(Solution().lengthOfLongestSubstring(" "))
+print(Solution().lengthOfLongestSubstring("hdcuwehfv hfcdnsjcnkj dhd kh jhfdjcjdhcjdhceuwhcadcknlndcen"))
