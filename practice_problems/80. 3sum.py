1662c77cccd19479b40e90ca7a6274d3ba1287e0
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 13:02:45 +0300

    robust test cases - one + two sum solution

--- practice_problems/80. 3sum.py
+++ practice_problems/80. 3sum.py
@@ -3,15 +3,26 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        visited = set()
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums) - 1):
-                for k in range(j + 1, len(nums)):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        ans = sorted([nums[i], nums[j], nums[k]])
-                        if tuple(ans) not in visited:
-                            visited.add(tuple(ans))
-                            res.append(ans)
-        return res
+        def twoSum(nums, invalid, target, visited, possibles):
+            hashmap = {}
+            for i, num in enumerate(nums):
+                if num in hashmap and i != invalid and hashmap[num] != invalid:
+                    # print(0 - target, nums[hashmap[num]], "====i====", hashmap[num], "invalid", invalid, " ", nums[i])
+                    possible = sorted([0 - target, nums[hashmap[num]], nums[i]])
+                    toVisit = tuple(possible)
+                    if toVisit not in visited:
+                        visited.add(toVisit)
+                        possibles.append(possible)
+                hashmap[target - num] = i
+            return possibles
         
+        res = []
+        visited = set()        
+        for i, num in enumerate(nums):
+            res = twoSum(nums, i, 0 - num, visited, res)         
+        return res
+
+# test = [-1,0,1,0,2,-1,1,0,2,-1,1,0,2,-1,0,2,-4,-1,0,1,0,2]
+test = [-1,0,1,2,-1,-4]
+test = [0, 0]
+print(Solution().threeSum(test))
