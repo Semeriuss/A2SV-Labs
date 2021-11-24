44043911484bd59f682d5195cdc60fa0a414c927
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 14:45:14 +0300

    cleaner and more efficient

--- practice_problems/80. 3sum.py
+++ practice_problems/80. 3sum.py
@@ -3,26 +3,33 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(nums, invalid, target, visited, possibles):
-            hashmap = {}
-            for i, num in enumerate(nums):
-                if num in hashmap and i != invalid and hashmap[num] != invalid:
-                    # print(0 - target, nums[hashmap[num]], "====i====", hashmap[num], "invalid", invalid, " ", nums[i])
-                    possible = sorted([0 - target, nums[hashmap[num]], nums[i]])
-                    toVisit = tuple(possible)
-                    if toVisit not in visited:
-                        visited.add(toVisit)
-                        possibles.append(possible)
-                hashmap[target - num] = i
-            return possibles
+        nums.sort()
         
-        res = []
-        visited = set()        
-        for i, num in enumerate(nums):
-            res = twoSum(nums, i, 0 - num, visited, res)         
+        N, res = len(nums), []
+        for i in range(N):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            sp, ep = i + 1, N - 1
+            target = nums[i] * -1
+            while sp < ep:
+                if nums[sp] + nums[ep] == target:
+                    res.append([nums[i], nums[sp], nums[ep]])
+                    sp += 1
+                    while sp < ep and nums[sp] == nums[sp - 1]:
+                        sp += 1
+                
+                elif nums[sp] + nums[ep] < target:
+                    sp += 1
+                
+                else:
+                    ep -= 1
+            
         return res
 
-# test = [-1,0,1,0,2,-1,1,0,2,-1,1,0,2,-1,0,2,-4,-1,0,1,0,2]
+
+test = [-1,0,1,0,2,-1,1,0,2,-1,1,0,2,-1,0,2,-4,-1,0,1,0,2]
 test = [-1,0,1,2,-1,-4]
 test = [0, 0]
 print(Solution().threeSum(test))
