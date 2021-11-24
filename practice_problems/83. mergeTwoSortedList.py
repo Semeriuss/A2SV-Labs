66b50b76e05a6e6c706d49d26c7db08a1c5c0c3e
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 21:27:39 +0300

    spaghetti

---
+++ practice_problems/83. mergeTwoSortedList.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        mergedList = ListNode()
+        head = mergedList
+        while list1 and list2:
+            if list1.val >= list2.val:
+                mergedList.val = list2.val
+                list2 = list2.next
+                if list2:
+                    mergedList.next = ListNode() 
+                    mergedList = mergedList.next
+            
+            elif list1.val < list2.val:
+                mergedList.val = list1.val
+                list1 = list1.next
+                if list1:
+                    mergedList.next = ListNode()
+                    mergedList = mergedList.next
+            
+        while list1:
+            mergedList.next = ListNode()
+            mergedList = mergedList.next
+            mergedList.val = list1.val
+            list1 = list1.next
+            if list1:
+                mergedList.next = ListNode()
+                mergedList = mergedList.next
+            
+        while list2:
+            mergedList.next = ListNode()
+            mergedList = mergedList.next
+            mergedList.val = list2.val
+            list2 = list2.next
+            if list2:
+                mergedList.next = ListNode()
+                mergedList = mergedList.next
+        
+        return head
+    
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
+
+list1 = ListNode()
+list2 = ListNode()
+ans = Solution().mergeTwoLists(list1, list2)
+
+
+while ans:
+    print(ans.val, end=" - ")
+    ans = ans.next
