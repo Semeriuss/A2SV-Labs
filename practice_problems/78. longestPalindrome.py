9a1f0c768100f77ebe5afa6da849e1b28bccbd91
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 08:46:38 +0300

    dp is also tle?

--- practice_problems/78. longestPalindrome.py
+++ practice_problems/78. longestPalindrome.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def isPalindrome(sp, ep):
@@ -16,12 +17,50 @@ class Solution:
                     break
         return ans
 
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+        dp = [[0 for _ in range(len(s))] for _ in range(len(s))] 
+        for i in range(len(s)):
+            dp[i][i] = True  
+            ans = s[i]    
+
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j]:
+                    if dp[i + 1][j - 1] or j - i == 1:
+                        dp[i][j] = True
+                        ans = s[i : j + 1] if len(s[i : j + 1]) >= len(ans) else ans
+        return ans
+    
+    # def longestPalindrome(self, s: str) -> str:
+    #     if len(s) < 2:
+    #         return s
+            
+    #     startPointer, endPointer = 0, 0
+    #     def findPalindrome(s, sp, ep):
+    #         nonlocal startPointer, endPointer
+    #         while sp >= 0 and ep < len(s) and s[sp] == s[ep]:
+    #                 ep += 1
+    #                 sp -= 1   
+
+    #         if endPointer < ep - sp - 1:
+    #             startPointer = sp + 1
+    #             endPointer = ep - sp - 1
+
+    #     for i in range(len(s) - 1):
+    #         findPalindrome(s, i, i)
+    #         findPalindrome(s, i, i + 1)
+            
+    #     return s[startPointer: startPointer + endPointer]
+
+
 
 s = "babad"
-s= "cbbd"
-s = "a"
-s = "ac"
-s = "cbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbd"
-print(len(s))
+# s= "cbbd"
+# s = "a"
+# s = "ac"
+# s = "cbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdcbbdbbdcbbdcbbdcbbdcbbdcbbdcbbd"
+# print(len(s))
+s = "cbcbbdacabcbbdacacaccabcabcadcaacacadcbbdacaacbbdacaccbbdacaacbbdacacbbcbbdacadcbbdacaacaca"
 
 print(Solution().longestPalindrome(s))
