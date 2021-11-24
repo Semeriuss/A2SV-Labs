d70e9551e65e105f4adfdccbc086de747bea296f
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 21:58:59 +0300

    merge sort basically

--- practice_problems/83. mergeTwoSortedList.py
+++ practice_problems/83. mergeTwoSortedList.py
@@ -7,49 +7,37 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
         mergedList = ListNode()
         head = mergedList
         while list1 and list2:
             if list1.val >= list2.val:
-                mergedList.val = list2.val
+                mergedList.next = ListNode(list2.val)
                 list2 = list2.next
-                if list2:
-                    mergedList.next = ListNode() 
-                    mergedList = mergedList.next
+                mergedList = mergedList.next
             
             elif list1.val < list2.val:
-                mergedList.val = list1.val
+                mergedList.next = ListNode(list1.val)
                 list1 = list1.next
-                if list1:
-                    mergedList.next = ListNode()
-                    mergedList = mergedList.next
+                mergedList = mergedList.next
             
         while list1:
-            mergedList.next = ListNode()
-            mergedList = mergedList.next
-            mergedList.val = list1.val
+            mergedList.next = ListNode(list1.val)
             list1 = list1.next
-            if list1:
-                mergedList.next = ListNode()
-                mergedList = mergedList.next
-            
-        while list2:
-            mergedList.next = ListNode()
             mergedList = mergedList.next
-            mergedList.val = list2.val
+
+        while list2:
+            mergedList.next = ListNode(list2.val)
             list2 = list2.next
-            if list2:
-                mergedList.next = ListNode()
-                mergedList = mergedList.next
+            mergedList = mergedList.next
         
+        head = head.next
         return head
     
 list1 = ListNode(1, ListNode(2, ListNode(4)))
 list2 = ListNode(1, ListNode(3, ListNode(4)))
 
-list1 = ListNode()
-list2 = ListNode()
+# list1 = ListNode()
+# list2 = ListNode()
 ans = Solution().mergeTwoLists(list1, list2)
 
 
