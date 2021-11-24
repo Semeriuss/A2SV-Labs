f18d0be21de8a30fb1c28312d3742cef44e6148e
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-24 22:06:05 +0300

    cleaner

--- practice_problems/83. mergeTwoSortedList.py
+++ practice_problems/83. mergeTwoSortedList.py
@@ -7,31 +7,11 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        mergedList = ListNode()
-        head = mergedList
-        while list1 and list2:
-            if list1.val >= list2.val:
-                mergedList.next = ListNode(list2.val)
-                list2 = list2.next
-                mergedList = mergedList.next
-            
-            elif list1.val < list2.val:
-                mergedList.next = ListNode(list1.val)
-                list1 = list1.next
-                mergedList = mergedList.next
-            
-        while list1:
-            mergedList.next = ListNode(list1.val)
-            list1 = list1.next
-            mergedList = mergedList.next
-
-        while list2:
-            mergedList.next = ListNode(list2.val)
-            list2 = list2.next
-            mergedList = mergedList.next
-        
-        head = head.next
-        return head
+        if not list1 or list2 and list1.val > list2.val:
+            list1, list2 = list2, list1
+        if list1:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
     
 list1 = ListNode(1, ListNode(2, ListNode(4)))
 list2 = ListNode(1, ListNode(3, ListNode(4)))
