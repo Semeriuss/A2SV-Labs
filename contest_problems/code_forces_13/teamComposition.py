811ae4f290ee8820e3c1c43451e26376f9503da8
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-25 22:43:44 +0300

    alas! it was a simple one

--- contest_problems/code_forces_13/teamComposition.py
+++ contest_problems/code_forces_13/teamComposition.py
@@ -1,26 +1,8 @@
-def teamComposition(mathematician, programmer):
-    a, b = (mathematician, programmer) if mathematician <= programmer else (programmer, mathematician)
-    count = 0
-    while a > 0 and b > 2:
-        a -= 1
-        b -= 3
-        a, b = (a, b) if a <= b else (b, a)
-        if a >= 0 and b >= 0:
-            count += 1
-    
-    return count
-
-def teamComposition(mathematician, programmer, memo):
-    a, b = (mathematician, programmer) if mathematician <= programmer else (programmer, mathematician)
-    if (a, b) in memo:
-        return memo[(a,b)]
-    if a <= 0 or b <= 0:
+def teamComposition(a, b):   
+    if a + b < 4:
         return 0
 
-    if a > 0 and b > 2:
-        memo[(a,b)] = 1 + teamComposition(a - 1, b - 3, memo)
-        return memo[(a,b)]
-    return 0
+    return min(a, b, (a + b)//4)
 
 
 
@@ -29,5 +11,5 @@ if __name__ == "__main__":
     res = []
     for i in range(t):
         mathematician, programmer = map(int, input().split())
-        res.append(teamComposition(mathematician, programmer, {}))
+        res.append(teamComposition(mathematician, programmer))
     print(*res, sep="\n")        
