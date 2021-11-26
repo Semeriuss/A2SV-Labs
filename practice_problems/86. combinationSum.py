9d4a0e1cb306391f60206e59ea20070b0f285d37
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-26 16:00:29 +0300

    surprising results in leetcode

--- practice_problems/86. combinationSum.py
+++ practice_problems/86. combinationSum.py
@@ -3,26 +3,14 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
         self.result = []
-        def findSum(target, res, index, memo):
-            if (target, index) in memo and memo[(target, index)] == 0:
-                self.result.append(memo[(target, index)])
-
-            if target == 0:
-                memo[(target, index)] = res
-                self.result.append(memo[(target, index)])
-
-            if target < 0 or index >= len(candidates):
-                memo[(target, index)] = None
-                return None
-
+        def findSum(target, res, index):
+            if target == 0: self.result.append(res)
+            if target < 0 or index >= len(candidates): return None
             for i in range(index, len(candidates)):
-                memo[(target, index)] = findSum(target - candidates[i], res + [candidates[i]], i, memo)     
-
-        findSum(target, [], 0, {})
+                findSum(target - candidates[i], res + [candidates[i]], i)     
+        findSum(target, [], 0)
         return self.result
-
 nums = [2, 3]
 target = 12
 print(Solution().combinationSum(nums, target))
