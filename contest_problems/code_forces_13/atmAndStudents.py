b11357222caefc02dcc063779e34587eab068646
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-26 16:57:45 +0300

    some bugs

--- contest_problems/code_forces_13/atmAndStudents.py
+++ contest_problems/code_forces_13/atmAndStudents.py
@@ -3,35 +3,36 @@ def maxService(amount, line):
     cost = amount
     sp, ep = 0, 0
 
-    while ep < len(line):
-        print(maxServed, (sp, ep), cost, line[ep])
+    while ep < len(line) and sp <= ep:
         if line[ep] >= 0:
             cost += line[ep]
+            maxServed = maxServed if maxServed[1] - maxServed[0] > ep - sp else (sp + 1, ep + 1)
             ep += 1
+        
         else:
             if abs(line[ep]) > cost:
-                maxServed = (sp, ep - 1) if (ep - sp) > (maxServed[1] - maxServed[0]) else maxServed
-                sp = ep + 1
-                cost = amount
+                maxServed = maxServed if maxServed[1] - maxServed[0] >= ep - sp - 1 else (sp + 1, ep)
+                if line[sp] >= 0:
+                    cost -= line[sp] 
+                else:
+                    cost += line[sp] * -1
+                    sp += 1
             else:
-                # maxServed = max(maxServed, (sp, ep))
-                maxServed = (sp, ep - 1) if (ep - sp) > (maxServed[1] - maxServed[0]) else maxServed
                 cost += line[ep]
-        maxServed = (sp, ep - 1) if (ep - sp) >= (maxServed[1] - maxServed[0]) else maxServed
-        ep += 1
-    # print(maxServed[0], maxServed[1], sum(line[maxServed[0]: maxServed[1] + 1]) + cost)
-    return (maxServed[0] + 1, maxServed[1] + 1) if sum(line[maxServed[0]: maxServed[1] + 1]) + cost >= 0 else -1
+                maxServed = maxServed if maxServed[1] - maxServed[0] > ep - sp else (sp + 1, ep + 1)
+                ep += 1
+        # print(line[ep], cost)
+    return f'{maxServed[0]} {maxServed[1]}' if maxServed != (0, 0) else -1
 
-amount = 10
-line = [-16, 2, -6, 8]
+if __name__ == "__main__":
+    t = int(input())
+    res = []
+    for i in range(t):
+        n, s = map(int, input().split())
+        line = list(map(int, input().split()))
+        res.append(maxService(s, line))
+    print(*res, sep="\n")
 
-# amount = 1000
-# line = [-100000, -100000, -100000]
-
-amount = 0
-line = [2, 6, -164, 1, -1, -6543]
-
-amount = 1
-line = [-1]
-print(maxService(amount, line))
-        
+# 1
+# 9 17
+# 8 -14 -7 -3 -3 3 10 -16 -5
