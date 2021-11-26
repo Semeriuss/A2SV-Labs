651fe4ccdbf3f577b1bc0441e2923bca3849ba4f
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-26 16:28:36 +0300

    another way

--- practice_problems/86. combinationSum.py
+++ practice_problems/86. combinationSum.py
@@ -3,14 +3,16 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.result = []
-        def findSum(target, res, index):
-            if target == 0: self.result.append(res)
-            if target < 0 or index >= len(candidates): return None
-            for i in range(index, len(candidates)):
-                findSum(target - candidates[i], res + [candidates[i]], i)     
-        findSum(target, [], 0)
-        return self.result
+        result = []
+        candidates.sort()
+        def findSum(remaining, store):
+            if remaining == 0: result.append(store)
+            for item in candidates:
+                if item > remaining: break
+                if store and store[-1] > item: continue
+                else: findSum(remaining - item, store + [item])
+        findSum(target, [])
+        return result
 nums = [2, 3]
 target = 12
 print(Solution().combinationSum(nums, target))
