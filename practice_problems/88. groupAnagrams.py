cd79de3c6f7b1a9e1349936f4cecb2f3c586e24f
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-27 16:56:49 +0300

    simple

--- practice_problems/88. groupAnagrams.py
+++ practice_problems/88. groupAnagrams.py
@@ -5,11 +5,8 @@ from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:  
         frequency = defaultdict(list)
-        for i in range(len(strs)):
-            count = [0]*26
-            for j in strs[i]:
-                count[ord(j) - ord('a')] += 1
-            frequency[tuple(count)].append(strs[i])
+        for word in strs:
+            frequency["".join(sorted(word))].append(word)
         return frequency.values()
 
 
