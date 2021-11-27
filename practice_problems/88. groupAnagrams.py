298251afc5cadeb97a37e0e061b3c61b96807e0e
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-27 17:18:38 +0300

    counting sort

--- practice_problems/88. groupAnagrams.py
+++ practice_problems/88. groupAnagrams.py
@@ -6,7 +6,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:  
         frequency = defaultdict(list)
         for word in strs:
-            frequency["".join(sorted(word))].append(word)
+            count = [0]*26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            sortedString = []
+            for i in range(26):
+                sortedString.append(chr(i + ord('a')) * count[i])
+            frequency["".join(sortedString)].append(word)
         return frequency.values()
 
 
