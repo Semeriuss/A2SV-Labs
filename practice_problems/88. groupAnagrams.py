42b7a6f8cbd04e6237c42190bc281f764baaf1f6
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-27 16:29:02 +0300

    only cubic

--- practice_problems/88. groupAnagrams.py
+++ practice_problems/88. groupAnagrams.py
@@ -5,27 +5,21 @@ from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         root = list(range(len(strs)))
-        
-        def find(node):
-            if root[node] != node:
-                root[node] = find(root[node])
-            return root[node]
-        
-        def union(node1, node2):
-            root1, root2 = find(node1), find(node2)
-            root[root2] = root1
-        
-        for i in range(len(strs) - 1):
-            checker = Counter(strs[i])
-            for j in range(i + 1, len(strs)):
-                if checker == Counter(strs[j]):
-                    union(i, j)
-        
-        group = defaultdict(list)
-        for i, n in enumerate(root):
-            group[n].append(strs[i])
+        group = []
+        visited = set()
+        for i in range(len(strs)):
+            if i not in visited:
+                visited.add(i)
+                checker = Counter(strs[i])
+                temp = [strs[i]]
+                for j in range(i, len(strs)):
+                    if j not in visited:
+                        if checker == Counter(strs[j]):
+                            temp.append(strs[j])
+                            visited.add(j)
+                group.append(temp)
     
-        return list(group.values())
+        return group
 
 
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
