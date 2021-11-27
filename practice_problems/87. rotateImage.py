9c5f70e9e0c0e14d87bc4d12c00ce3190a8a2ea3
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-27 15:46:33 +0300

    ~ is n - 1 - i, quadrants based

--- practice_problems/87. rotateImage.py
+++ practice_problems/87. rotateImage.py
@@ -6,10 +6,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix[:] = zip(*matrix[::-1])
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(n-n//2):
+                matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = \
+                    matrix[~j][i], matrix[~i][~j], matrix[j][~i], matrix[i][j]
+        
+        return matrix
+        
     
 mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-mat = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+# mat = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 print(Solution().rotate(mat))
 
 
