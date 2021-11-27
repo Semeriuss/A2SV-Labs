b1c78446feab2cd9881ff5f54a0f102fdbce7c7a
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-27 15:26:02 +0300

    python one-liner

--- practice_problems/87. rotateImage.py
+++ practice_problems/87. rotateImage.py
@@ -1,26 +1,12 @@
 from typing import List
 
 
-class Solution:
+class Solution:    
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
-        left, right = 0, len(matrix) - 1
-        while left < right:
-            top, bottom = left, right
-            for level in range(right - left):
-
-                topLeft = matrix[top][left + level]
-
-                matrix[top][left + level] = matrix[bottom - level][left]
-                matrix[bottom - level][left] = matrix[bottom][right - level]
-                matrix[bottom][right - level] = matrix[top + level][right]
-                matrix[top + level][right] = topLeft
-
-            left += 1
-            right -= 1
+        matrix[:] = zip(*matrix[::-1])
     
 mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 mat = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
