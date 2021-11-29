727e9493ab00be8201f64a5a7cb1b2d03f13e347
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-29 22:09:45 +0300

    same way, makes more sense

--- practice_problems/95. uniquePaths.py
+++ practice_problems/95. uniquePaths.py
@@ -1,29 +1,29 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        rPath = [(-1, 0), (0, -1)]
+        self.path = [(0, 1), (1, 0)]
+        return self.countPath((0, 0), (m - 1, n - 1), defaultdict(int))
 
-        def fitsBoundary(point):
-            row, col = point
-            return 0 <= row < m and 0 <= col < n
-
-        def countPath(dst, memo):
-            if dst in memo:
-                return memo[dst]
-            if dst == (0, 0):
-                return 1
-            count = 0
-            if fitsBoundary(dst):
-                x, y = dst
-                for path in rPath:
-                    path_x, path_y = path
-                    next_x, next_y = x + path_x, y + path_y
-                    if fitsBoundary((next_x, next_y)):
-                        count += countPath((next_x, next_y), memo)
-                    memo[(x, y)] = count
-            return count
-        return countPath((m - 1, n - 1), defaultdict(int))
+    def fitsBoundary(self, point, boundary):
+        x, y = point
+        a, b = boundary
+        return 0 <= x < a and 0 <= y < b
+    
+    def countPath(self, src, dst, memo):
+        final_x, final_y = dst
+        if src in memo:
+            return memo[src]
+        if src == (final_x, final_y):
+            return 1
+        curr_x, curr_y = src
+        count = 0
+        for path_x, path_y in self.path:
+            next_x, next_y = path_x + curr_x, path_y + curr_y
+            if self.fitsBoundary((next_x, next_y), (final_x + 1, final_y + 1)):
+                count += self.countPath((next_x, next_y), dst, memo)
+        memo[(curr_x, curr_y)] = count
+        return memo[src]
 print(Solution().uniquePaths(100, 100))
 
                     
