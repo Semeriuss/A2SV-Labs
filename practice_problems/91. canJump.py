bbc26f5d026955ab5f7bde5f8c36723786e8fccb
Semere Habtu <se.semere.habtu@gmail.com>
2021-11-29 11:20:10 +0300

    clean up

--- practice_problems/91. canJump.py
+++ practice_problems/91. canJump.py
@@ -1,47 +1,54 @@
 from collections import deque
 from typing import List
-
-
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         self.res = False
-        def hasPath(pos, visited):
-            if pos >= len(nums) - 1:
-                self.res = True
-                return self.res
-            
-            if len(visited) == len(nums):
-                return 
-            visited.add(pos)
-            for i in range(1, nums[pos] + 1):
-                if i >= len(nums) - 1:
-                    self.res = True
+        def dfs(index):
+            res = False
+            def dfs_helper(index, visited):
+                nonlocal res 
+                if index >= len(nums) - 1:
+                    res = True
                     return True
-                if i not in visited and hasPath(i, visited):
-                    self.res = True
-                    return True
-            self.res = False
-            return self.res
-        return hasPath(0, set())
 
-        def hasPath(src):
-            queue = deque()
-            visited = set()
-            queue.append(src)
-            visited.add(src)
+                if len(visited) == len(nums):
+                    return 
+
+                visited.add(index)
+                for jump in range(1, nums[index] + 1):
+                    maxJump = index + jump
+                    if maxJump >= len(nums) - 1:
+                        res = True
+                        return 
+                    if maxJump not in visited and dfs(maxJump):
+                        return True
+                return False
+            res = dfs_helper(index, set())
+            return res
+        
+        def bfs(index):
+            queue = deque([index])
+            visited = set([index])
+            
             while queue:
                 currPos = queue.popleft()
-                maxJump = currPos + nums[currPos] + 1              
-                for i in range(currPos + 1, maxJump):
-                    if i >= len(nums):
+                if currPos >= len(nums) - 1:
+                    return True
+                maxJump = currPos + nums[currPos] + 1
+                for jump in range(currPos + 1, maxJump):
+                    if jump >= len(nums):
                         return True
-                    if i not in visited and i < len(nums):
-                        visited.add(i)
-                        queue.append(i)
+                    if jump not in visited and jump < len(nums):
+                        queue.append(jump)
+                        visited.add(jump)
             return False
-        return hasPath(0)
+        # self.res = bfs(0)
+        self.res = dfs(0)
+        return self.res
 
 nums = [2, 3, 1, 1, 4]
+nums = [1, 1, 1, 0]
+# nums = [0]
 # nums = [3,2,1,0,4]
 # nums = [2, 0]
 # nums = [2, 5, 0, 0]
