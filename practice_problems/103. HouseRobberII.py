4053eb57c1ccde5d48c46085cf2e2968fa470a71
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-01 13:06:14 +0300

    feels like cheating

--- practice_problems/103. HouseRobberII.py
+++ practice_problems/103. HouseRobberII.py
@@ -5,14 +5,16 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
         if N == 1: return nums[0]
-        reordered_nums = tuple([nums[-1]] + nums[:-1])
         @lru_cache(None)
-        def dfs(index, houses):
-            if index >= N: return 0
-            return max(houses[index] + dfs(index + 2, houses), dfs(index + 1, houses))
-        
-        return dfs(0, reordered_nums)
+        def dfs(index, houses, BOUND):
+            if index >= BOUND: return 0
+            return max(nums[index] + dfs(index + 2, houses, BOUND), dfs(index + 1, houses, BOUND))
+            
+        houses = tuple(nums)
+        return max(dfs(1, houses, N), dfs(0, houses[:-1], N - 1))
 
 nums = [9, 1, 4, 9, 1, 0]
 nums = [1, 2, 3, 4, 5]
+nums = [9, 1, 4, 9, 1, 0,1, 2, 3, 4, 5,1, 2, 3, 4, 5, 9, 1, 4, 9, 1, 9, 1, 4, 9, 1, 0,1, 2, 3, 4, 5,1, 2, 3, 4, 5, 9, 1, 4, 9, 1, 0,1, 2, 3, 4, 5,1, 2, 3, 4, 5,1, 2, 3, 4, 5, 9, 1, 4, 9, 1, 0,1, 2, 3, 4, 5,1, 2, 3, 4, 5,1, 2, 3, 4, 5, 9, 1, 4, 9, 1, 0,1, 2, 3, 4, 5,1, 2, 3, 4, 5,1, 2, 3, 4, 5, 1, 2, 3, 4, 5,]
+print(len(nums))
 print(Solution().rob(nums))
