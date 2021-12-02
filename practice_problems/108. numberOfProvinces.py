5f8cb847cfa2a2dbc2ef6dea834585756ce689d1
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-02 11:31:03 +0300

    just dfs would work apparently

--- practice_problems/108. numberOfProvinces.py
+++ practice_problems/108. numberOfProvinces.py
@@ -1,30 +1,29 @@
 from typing import List
 
-
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        root = list(range(len(isConnected)))
-        size = [1] * len(isConnected)
+        N = len(isConnected)
 
-        def find(node):
-            if node != root[node]:
-                root[node] = find(root[node])
-            return root[node]
+        visited = [False] * N
+
+        def dfs(col):
+            for row in range(N):
+                if isConnected[row][col] and not visited[row]:
+                    visited[row] = True
+                    dfs(row)
         
-        def union(node1, node2):
-            root1, root2 = sorted([find(node1), find(node2)], key=lambda x: size[x])
-            root[root1] = root2
-            size[root2] += size[root1]
+        provinces = 0
+        for col in range(N):
+            if not visited[col]:
+                provinces += 1
+                visited[col] = True
+                dfs(col)
         
-        def rootConnected(node1, node2):
-            return find(node1) == find(node2)
-        
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if i != j and (isConnected[i][j] or rootConnected(i, j)):
-                    union(i, j)
-        print(root)
-        return len(set(root))
+        return provinces
+                
+                    
+
+            
 
 isConnected = [
                 [1,1,1,0,1,1,1,0,0,0],
@@ -55,5 +54,11 @@ isConnected = [
                 [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
                 [0,0,0,0,0,0,1,0,1,0,0,0,0,1,0],
                 [0,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
+            ]
+
+isConnected = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
             ]
 print(Solution().findCircleNum(isConnected))
