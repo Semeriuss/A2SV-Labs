b6442e7163ed7424125770152a1db74f1674acf5
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-04 09:41:19 +0300

    sleek recursion - modifies input

--- practice_problems/117. jumpGameIII.py
+++ practice_problems/117. jumpGameIII.py
@@ -4,25 +4,7 @@ from collections import deque
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
 
-        canJump = False
-        N = len(arr)
-
-        def fitsBoundary(index):
-            return 0 <= index < N
-
-        que = deque([start])
-        visited = set()
-        while que:
-            curr_pos = que.popleft()
-
-            if fitsBoundary(curr_pos) and not arr[curr_pos]:
-                canJump = True
-                break
-            
-            if fitsBoundary(curr_pos) and (curr_pos, arr[curr_pos]) not in visited:
-                visited.add((curr_pos, arr[curr_pos]))
-                que.append(curr_pos + arr[curr_pos])
-                que.append(curr_pos - arr[curr_pos])
-            
-        return canJump
-        
+        if 0 <= start < len(arr) and arr[start] >= 0:
+            arr[start] = -arr[start]
+            return not arr[start] or self.canReach(arr, start - arr[start]) or self.canReach(arr, start + arr[start])
+        return False
