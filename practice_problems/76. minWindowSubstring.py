2558e03c24b8d2a9a9f229e8c819ecdb61753bb8
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-04 12:10:32 +0300

    two pointers, hash map, string

--- practice_problems/76. minWindowSubstring.py
+++ practice_problems/76. minWindowSubstring.py
@@ -1,39 +1,41 @@
-
-
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
         N = len(s)
-
-        t_dict = Counter(t)
-        s_dict = defaultdict(int)
-
-        def matches(t, s):
-            for x in t:
-                if s[x] < t[x]:
-                    return False
-            return True
-
-        start, end = 0, 0
-        res = ""
-        while start <= end and end < N:
-
-            if matches (t_dict, s_dict):
-                res = s[start : end + 1] if len(s[start : end + 1]) < len(res) or len(res) < len(t) else res
-                if s[start] in t_dict: s_dict[s[start]] -= 1
+        
+        if not s or not t or len(t) > N: return ''
+        
+        t_count = Counter(t)
+        s_count = Counter()
+        
+        t_chars = len(t_count)
+        matching_chars = 0
+        
+        start, end = 0, -1
+        res = ''
+        while start < N:
+            
+            if matching_chars < t_chars:
+                if end == N - 1: return res
+                end += 1
+                s_count[s[end]] += 1
+                if t_count[s[end]] > 0 and s_count[s[end]] == t_count[s[end]]:
+                    matching_chars += 1
+                    
+            else:
+                s_count[s[start]] -= 1
+                if t_count[s[start]] > 0 and s_count[s[start]] == t_count[s[start]] - 1:
+                    matching_chars -= 1
                 start += 1
             
-            elif s[end] in t_dict:
-                s_dict[s[end]] += 1
-                if matches(t_dict, s_dict):
-                    res = s[start : end + 1] if len(s[start : end + 1]) < len(res) or len(res) < len(t) else res
-                
-                if end < N - 1:
-                    end += 1
-                else: 
-                    start += 1
-            
-            else:
-                end += 1
-        
+            if matching_chars == t_chars:
+                if not res:
+                    res = s[start : end + 1]
+                else:
+                    if end - start + 1 < len(res):
+                        res = s[start : end + 1]
         return res
+                
+        
+        
