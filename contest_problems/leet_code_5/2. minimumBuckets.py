51ddd1d616731fe705505865e859c7734db08951
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-05 13:38:03 +0300

    spaghetti

--- contest_problems/leet_code_5/2. minimumBuckets.py
+++ contest_problems/leet_code_5/2. minimumBuckets.py
@@ -6,33 +6,70 @@ class Solution:
         minBuckets = 0
         visited = set()
         while i < N:
+            if i in visited or i - 1 in visited or i + 1 in visited:
+                    i += 1
+                    continue
             if i > 0 and i < N - 1:
-                if street[i] == 'H':
-                    if street[i - 1] != '.' and street[i + 1] != '.':
-                        return -1
-                    elif street[i - 1] != '.':
-                        return -1
-                    elif street[i + 1] != '.':
-                        minBuckets += 1
-                        i += 2
-                    else:
-                        minBuckets += 1
-                        i += 1
+                if street[i] == 'H' and street[i - 1] == 'H' and street[i + 1] == 'H':
+                    return -1
+                elif street[i] == 'H' and street[i - 1] == 'H' and street[i + 1] == '.':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 3
+                if street[i] == 'H' and street[i - 1] == '.' and street[i + 1] == 'H':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 1
+                elif street[i] == 'H' and street[i - 1] == '.' and street[i + 1] == '.':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 3
+                elif street[i] == '.' and street[i - 1] == 'H' and street[i + 1] == 'H':
+                    visited.add(i + 1)
+                    minBuckets += 1
+                    i += 3
+                elif street[i] == '.' and street[i - 1] == 'H' and street[i + 1] == '.':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 2
+                elif street[i] == '.' and street[i - 1] == '.' and street[i + 1] == 'H':
+                    i += 1
+                elif street[i] == '.' and street[i - 1] == '.' and street[i + 1] == '.':
+                    i += 1
                 else:
-                    if street[i - 1] == '.' and i - 1 not in visited:
-                        i += 1
-                    elif street[i + 1] == '.':
-                        visited.add(i)
-                        minBuckets += 1
-                        i += 1
-                    else:
-                        minBuckets += 1
-                        i += 2
-            
-            else:
-                i += 1
+                    i += 1
+            elif i == 0:
+                if street[i] == 'H' and i + 1 < N and street[i + 1] == 'H':
+                    return -1
+                elif street[i] == 'H' and i + 1 < N and street[i + 1] == '.':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 2
+                elif street[i] == 'H' and i + 1 >= N:
+                    return -1
+                elif street[i] == '.' and street[i + 1] == 'H':
+                    visited.add(i + 1)
+                    minBuckets += 1
+                    # print("hi")
+                    i += 3 if  i + 3 < N else 2
+                else:
+                    i += 1
+            elif i == N - 1:
+                if street[i] == 'H' and i - 1 > 0 and street[i - 1] == 'H':
+                    return -1
+                elif street[i] == 'H' and i - 1 > 0 and street[i - 1] == '.':
+                    visited.add(i)
+                    minBuckets += 1
+                    i += 1
+                else:
+                    i += 1
+            print(i - 1, i, i + 1, minBuckets)
         return minBuckets
 
 street = "H..H"
 street = ".H.H."
+street = ".HHH."
+street = "H"
+street = "."
+street = ".HH.H.H.H.."
 print(Solution().minimumBuckets(street))
