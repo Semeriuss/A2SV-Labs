b6184b9e818311c10610d03cfe36808b3a91f0cc
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-10 00:05:42 +0300

    shorter

--- practice_problems/125. nextGreaterNodeInLinkedList.py
+++ practice_problems/125. nextGreaterNodeInLinkedList.py
@@ -8,16 +8,12 @@ class ListNode:
         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        
-        root, nodes = head, []
-        while root:
-            nodes.append(root.val)
-            root = root.next
-        
-        answer, stack = [0]*len(nodes), [nodes[len(nodes) - 1]]
-        for i in range(len(nodes) - 1, - 1, -1):
-            while stack and nodes[i] >= stack[-1]:
-                stack.pop()
-            if stack: answer[i] = stack[-1]
-            stack.append(nodes[i])
+                
+        answer, stack = [], []
+        while head:
+            while stack and stack[-1][1] < head.val:
+                answer[stack.pop()[0]] = head.val
+            stack.append([len(answer), head.val])
+            answer.append(0)
+            head = head.next
         return answer
