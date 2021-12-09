5291df23c2ac32c41a738ec4556f50adc5bc9eda
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-09 21:22:40 +0300

    sleeker way

--- practice_problems/124. addTwoNumbers.py
+++ practice_problems/124. addTwoNumbers.py
@@ -9,23 +9,13 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        dummy = ListNode()
+        dummy = end = ListNode()
+        addends = l1, l2
+        carry = 0
+        while addends or carry:
+            carry += sum([a.val for a in addends])
+            addends = [a.next for a in addends if a.next]
+            end.next = end = ListNode(carry % 10)
+            carry //= 10
+        return dummy.next
 
-        head, temp = dummy, 0
-
-        while l1 and l2:
-            dummy.next, temp = ListNode((l1.val + l2.val + temp)%10), ListNode((l1.val + l2.val + temp)//10)
-            l1, l2, dummy = l1.next, l2.next, dummy.next
-        
-        while l1:
-            dummy.next, temp = ListNode((l1.val + temp)%10), ListNode((l1.val + temp)//10)
-            l1, dummy = l1.next, dummy.next
-        
-        while l2:
-            dummy.next, temp = ListNode((l2.val + temp)%10), ListNode((l2.val + temp)//10)
-            l2, dummy = l2.next, dummy.next
-        
-        if temp:
-            dummy.next = ListNode(temp)
-            
-        return head.next
