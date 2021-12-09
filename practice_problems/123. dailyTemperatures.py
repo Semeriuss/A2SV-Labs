bab86e08a835c643238ed52fb7c72eeb7da57baa
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-09 22:42:59 +0300

    stack

--- practice_problems/123. dailyTemperatures.py
+++ practice_problems/123. dailyTemperatures.py
@@ -4,14 +4,15 @@ from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         answer = [0]*len(temperatures)
-        for i in range(len(temperatures)):
-            count = 0
-            for j in range(i + 1, len(temperatures)):
-                if temperatures[j] > temperatures[i]:
-                    count += 1
-                    answer[i] = count
-                    break
-                else:
-                    count += 1
+        stack = [len(temperatures) - 1]
+        for i in range(len(temperatures) - 1, - 1, -1):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                stack.pop()
+            if stack: answer[i] = stack[-1] - i
+            stack.append(i)
+                
         return answer
+
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+print(Solution().dailyTemperatures(temperatures))
                     
