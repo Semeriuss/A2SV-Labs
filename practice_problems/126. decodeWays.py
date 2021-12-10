7edf1dac779c39d16bc510c6ad94863556c42fdf
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-10 22:55:09 +0300

    bottom up dp

--- practice_problems/126. decodeWays.py
+++ practice_problems/126. decodeWays.py
@@ -3,12 +3,13 @@ class Solution:
     @lru_cache(maxsize = None)
     def numDecodings(self, s: str) -> int:
         if s and s[0] == '0': return 0
-        if len(s) == 1 or s == "": return 1
-
-        if len(s) >= 2:
-            if s[0 : 2] <= '26':
-                return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
-            return self.numDecodings(s[1:])
-
+        dp = [0 for _ in range(len(s) + 1)]
+        dp[0 : 2] = [1, 1]
+        for i in range(2, len(s) + 1):
+            if 0 < int(s[i - 1: i]) <= 9:
+                dp[i] += dp[i - 1]
+            if 10 <= int(s[i - 2: i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[-1]
 s = "12"
 print(Solution().numDecodings(s))
