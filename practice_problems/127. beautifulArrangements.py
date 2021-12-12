ff172b5f2ff72d82cf6788192edbcd4739c8a832
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-12 20:54:44 +0300

    optimized method

--- practice_problems/127. beautifulArrangements.py
+++ practice_problems/127. beautifulArrangements.py
@@ -3,24 +3,12 @@ from typing import List
 
 class Solution:
     def countArrangement(self, n: int) -> int:
-        self.count = 0
-        nums = list(range(1, n + 1))
-        self.permuteAndCount(nums, 0, 0, False)
-        return self.count
+        return self.fastCount(set(range(1, n + 1)), n)
 
-
-    def permuteAndCount(self, nums: List, i: int , count: int, beautifulOrder: bool) -> List:
-        if i == len(nums):
-            if beautifulOrder:
-                self.count += 1
-        else:
-            perm = nums[:]
-            for j in range(i, len(nums)):
-                perm[i], perm[j] = perm[j], perm[i]
-                if not (i + 1) % perm[i] or not perm[i] % (i + 1):
-                    beautifulOrder = True
-                    self.permuteAndCount(perm, i + 1, count, beautifulOrder)
-        return count
+    def fastCount(self, nums, i):
+        if i == 1:
+            return 1
+        return sum(self.fastCount(nums - {remNums}, i - 1) for remNums in nums if not remNums % i or not i % remNums)
 
 
 print(Solution().countArrangement(15))
