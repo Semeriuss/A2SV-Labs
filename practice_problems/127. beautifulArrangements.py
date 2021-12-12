7647fb3ea6ba7be3a108a228c1a3d82e47d34b03
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-12 21:03:51 +0300

    complicated bitmask method

--- practice_problems/127. beautifulArrangements.py
+++ practice_problems/127. beautifulArrangements.py
@@ -3,12 +3,14 @@ from typing import List
 
 class Solution:
     def countArrangement(self, n: int) -> int:
-        return self.fastCount(set(range(1, n + 1)), n)
-
-    def fastCount(self, nums, i):
-        if i == 1:
-            return 1
-        return sum(self.fastCount(nums - {remNums}, i - 1) for remNums in nums if not remNums % i or not i % remNums)
-
+        return self.bitMaskCount(n, 0, n)
+    
+    def bitMaskCount(self, N: int, bitmask: int, index: int):
+        if index == 0: return 1
+        order = 0
+        for i in range(N):
+            if not bitmask&1<<i and (not (i + 1)%index or not index%(i + 1)):
+                order += self.bitMaskCount(N, bitmask^1<<i, index - 1)
+        return order
 
 print(Solution().countArrangement(15))
