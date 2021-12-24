474fc4caaac39ac173986f45f9bbdee20ea37d36
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-24 13:41:49 +0300

    even sleeker

--- practice_problems/139. replaceWord.py
+++ practice_problems/139. replaceWord.py
@@ -1,27 +1,12 @@
-import collections
 from typing import List
        
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        _trie = lambda: collections.defaultdict(_trie) 
-        root = _trie()
-        
-        END = True
-        for word in dictionary:
-            current = root
-            for c in word:
-                if c not in current: current[c] = _trie()
-                current = current[c] 
-            current[END] = word
-        
+        roots = set(dictionary)
         def replace(word):
-            current = root
-            for c in word:
-                if c not in current: break
-                current = current[c]
-                if END in current: return current[END]
+            for i in range(len(word)):
+                if word[:i] in roots: return word[:i]
             return word
-        
         return " ".join(map(replace, sentence.split()))
                 
 
