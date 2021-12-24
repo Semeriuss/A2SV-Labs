0cc25b691e21912e58c34730b77ab48b07f38d0a
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-24 11:46:35 +0300

    Trie implementation

---
+++ basic_algorithms/trie.py
@@ -0,0 +1,26 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        current = self.root
+        for c in word:
+            if c not in current:
+                current.children[c] = TrieNode()
+            current = current.children[c]
+        current.isEndOfWord = True
+
+    def search(self, word: str, isPrefix: bool = False) -> bool:
+        current = self.root
+        for c in word:
+            if c not in word: return False
+            current = current.children[c]
+        return current.isEndOfWord or isPrefix
+
+    def prefix(self, prefix: str) -> bool:
+        return self.search(prefix, True)
