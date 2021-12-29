7532c4502237f54a2e5cb8c1eab4751751df7c3e
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-29 21:39:02 +0300

    This works!

--- contest_problems/code_forces_15/B.py
+++ contest_problems/code_forces_15/B.py
@@ -1,8 +1,9 @@
 def smallestMirror(s):
-    if len(s) == 1: return ""
     res = s[0]
     for i in range(1, len(s)):
         if ord(s[i]) < ord(s[i - 1]):
+            res += s[i]
+        elif ord(s[i]) == ord(s[i - 1]) and s[0] != s[i]:
             res += s[i]
         else:
             break
