dcb6f1cdf812a473a754b28bc87276fd0f0ab201
Semere Habtu <se.semere.habtu@gmail.com>
2021-12-31 10:44:50 +0300

    works

--- practice_problems/maxAnscestorDiff.py
+++ practice_problems/maxAnscestorDiff.py
@@ -10,17 +10,11 @@ class TreeNode:
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        self.min, self.max = float('inf'), float('-inf')
         def dfs(node, minsofar, maxsofar):
-            if not node.left and not node.right: return node.val
-            if node.left:
-                minsofar = min(minsofar, node.left.val)
-                maxsofar = max(maxsofar, node.left.val)
-                self.res = max(self.res, dfs(node.left, minsofar, maxsofar))
-            if node.right:
-                minsofar = min(minsofar, node.right.val)
-                maxsofar = max(maxsofar, node.right.val)
-                self.res = max(self.res, dfs(node.right, minsofar, maxsofar))
-            return max(abs(node.val - minsofar), abs(node.val - maxsofar))
-        dfs(root, float("inf"), float("-inf"))
+            if not node: return
+            self.res = max(self.res, abs(node.val - minsofar), abs(node.val - maxsofar))
+            minsofar, maxsofar = min(minsofar, node.val), max(maxsofar, node.val)
+            dfs(node.left, minsofar, maxsofar)
+            dfs(node.right, minsofar, maxsofar)
+        dfs(root, root.val, root.val)
         return self.res
