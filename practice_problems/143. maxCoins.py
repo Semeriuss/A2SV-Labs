add581e6f708a3cfb76e4b3852f92d1f4631ee57
Semere Habtu <se.semere.habtu@gmail.com>
2022-01-01 22:03:45 +0300

    wtf?

--- practice_problems/143. maxCoins.py
+++ practice_problems/143. maxCoins.py
@@ -4,28 +4,17 @@ from operator import __mul__
 
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        balloons = [1] + nums + [1]
-        self.res = 0
-        def dfs(balloon, res):
-            if len(balloon) < 3:
-                return balloon[1] * balloon[0]
-            print(self.res, res, balloon)
-            for i in range(1, len(balloon)):
-                tempV, tempB = self.burst(balloon, i)
-                dfs(tempB, res + tempV)
-                self.res = max(self.res, res)
-            
-            self.res = max(self.res, res)
-            return res
-        dfs(balloons, 0)
-        return self.res
+        nums = [1] + [num for num in nums if num > 0] + [1]
+        n = len(nums)
+        dp = [[0]*n for _ in range(n)]
 
-    
-    def burst(self, balloons, indexToRemove):
-        # if len(balloons) < 3: return balloons[1], [1, 1]
-        res = reduce(__mul__, balloons[indexToRemove - 1: indexToRemove + 2])
-        modified = balloons[:indexToRemove] + balloons[indexToRemove + 1:] 
-        return res, modified
+        for k in range(2, n):
+            for left in range(0, n - k): 
+                right = left + k   
+                for i in range(left + 1, right): 
+                    dp[left][right] = max(dp[left][right], nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right])
+        return dp[0][n - 1]
+
 
 nums = [3,1,5,8]
 nums = [4,6,7,1]
