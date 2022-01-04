23633a00bb56b428f85963d2d2fa1c13c8bb9311
Semere Habtu <se.semere.habtu@gmail.com>
2022-01-04 12:50:26 +0300

    sleek

--- practice_problems/145. letterCasePermutation.py
+++ practice_problems/145. letterCasePermutation.py
@@ -3,25 +3,12 @@ from typing import List
 
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        self.res = []
+        res = [""]
+        for c in s:
+            res = [x + cc for x in res for cc in {c, c.swapcase()}]
+        return res
         
-        def permute(i, path):
-            if i < len(s):
-                if s[i].isalpha():
-                    permute(i + 1, path + [s[i].upper()])
-                    permute(i + 1, path + [s[i].lower()])
-                
-                else:
-                    permute(i + 1, path + [s[i]])
-            
-            elif i == len(s):
-                self.res.append("".join(path))
-                return 
-            else:
-                return 
         
-        permute(0, [])
-        return self.res
 
 s= "a1b3d"
 print(Solution().letterCasePermutation(s))
