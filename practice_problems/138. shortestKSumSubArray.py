8dd7657f8b51579b491dce2451d903bce6944c19
Semere Habtu <se.semere.habtu@gmail.com>
2022-01-06 11:15:44 +0300

    montonic queue

--- practice_problems/138. shortestKSumSubArray.py
+++ practice_problems/138. shortestKSumSubArray.py
@@ -1,32 +1,22 @@
 from typing import List
-
+from collections import deque
 
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        self.minlen = float("inf")
+        res = n + 1
         
         prefixsum = [0]
         for i, num in enumerate(nums):
             prefixsum.append(prefixsum[i] + num)
         
-        def findMinSub(sp, ep, arr, k, dp):
-            if (sp, ep) in dp: return dp[(sp, ep)]
-            if sp > ep or ep < 0 or ep >= len(arr) or sp < 0 or sp >= len(arr):
-                dp[(sp, ep)] = -1
-                return 
-            
-            if arr[ep] - arr[sp] >= k:
-                dp[(sp, ep)] = ep - sp
-                self.minlen = min(self.minlen, ep - sp)
-            
-            dp[(sp, ep)] = findMinSub(sp + 1, ep, arr, k, dp)
-            dp[(sp, ep)] = findMinSub(sp, ep - 1, arr, k, dp)
-            
-        
-        findMinSub(0, len(prefixsum) - 1, prefixsum, k, {})
+        d = deque()
+        for i in range(n + 1):
+            while d and prefixsum[i] - prefixsum[d[0]]: res = min(res, i - d.popleft())
+            while d and prefixsum[i] <= prefixsum[d[-1]]: d.pop()
+            d.append(i)
 
-        return self.minlen if self.minlen != float("inf") else -1
+        return res if res != n + 1 else -1
 
 """ 
 [1]
