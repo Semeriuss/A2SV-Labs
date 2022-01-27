b88f4f303ddab06a64ec10244aebfc91de4f09b0
Semere Habtu <se.semere.habtu@gmail.com>
2022-01-27 22:10:09 +0300

    shorter line of codes

--- practice_problems_2/1. maximumXORFromPair.py
+++ practice_problems_2/1. maximumXORFromPair.py
@@ -5,28 +5,20 @@ class Solution:
 
         trie = {}
         maxbits = len(bin(max(nums))) - 2
-        
-        for num in nums:
-            num_in_bin = bin(num).lstrip('0b')
-            if len(num_in_bin) < maxbits: num_in_bin = '0'*(maxbits - len(num_in_bin)) + num_in_bin
-            current = trie
-            for bit in num_in_bin:
-                if bit not in current: current[bit] = {}
-                current = current[bit]
-            current['#'] = num
-
         maxor = 0
         for num in nums:
             num_in_bin = bin(num).lstrip('0b')
             if len(num_in_bin) < maxbits: num_in_bin = '0'*(maxbits - len(num_in_bin)) + num_in_bin
-            current = trie
+            current, operation = trie, trie
             for bit in num_in_bin:
                 desired = int(not int(bit))
-                if str(desired) in current: current = current[str(desired)]
-                elif str(int(not desired)) in current: current = current[str(int(not desired))]
-            if '#' in current: 
-                maxor = max(maxor, num^current['#'])
-
+                if str(desired) in operation: operation = operation[str(desired)]
+                elif str(int(not desired)) in operation: operation = operation[str(int(not desired))]
+                if bit not in current: current[bit] = {}
+                current = current[bit]
+            current['#'] = num
+            if '#' in operation: 
+                maxor = max(maxor, num^operation['#'])
         return maxor
     
 nums = [3, 25, 10, 5, 2, 8]
