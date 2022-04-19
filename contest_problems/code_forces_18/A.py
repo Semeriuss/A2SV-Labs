80e0569cd313ff0f9d9d726ad0755bbcac6ac98c
Semere Habtu <se.semere.habtu@gmail.com>
2022-04-19 19:09:52 +0300

    pretests passed!!!! :  )

--- contest_problems/code_forces_18/A.py
+++ contest_problems/code_forces_18/A.py
@@ -3,23 +3,15 @@ def solve(n, m):
     if a > 1 and b == 0 or b > 1 and a == 0:
         return -1
     count = 0
-    while a > 0 and b > 0:
-        a -= 1
-        b -= 1
-        count += 2
-    if a > 0:
-        if a % 2 == 0:
-            count += (a * 2) 
-        else:
-            count += (a * 2) - 1
-    elif b > 0:
-        if a % 2 == 0:
-            count += (b * 2)
+    a, b = min(a, b), max(a, b)
+    if b > 0:
+        count = a*2
+        b -= a
+        if b % 2 == 0:
+            count += (b * 2) 
         else:
             count += (b * 2) - 1
-    
     return count
-
 
 if __name__ == "__main__":
     t = int(input())
