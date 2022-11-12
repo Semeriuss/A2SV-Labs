7924257d438546ff8e86128a31c6d0f5c0dc8787
Semere Habtu <se.semere.habtu@gmail.com>
2022-11-12 18:18:56 +0300

    divide and conquer

--- contest_problems/code_forces_25/D.py
+++ contest_problems/code_forces_25/D.py
@@ -1,30 +1,27 @@
 for _ in range(int(input())):
     m = int(input())
     p = list(map(int, input().split()))
-
+    p = [0] + p
     count = 0
     j = 1
-    i = 1
     INVALID = False
-    while j < m:
-        k = i
-        print("---", i, j, p)
-        while i < m:
-            if p[i] < p[i - j]:
-                if p[i - j] - p[i] != j:
-                    print("ERROR",p, p[i - j], p[i], j, i)
-                    INVALID = True 
-                    break 
-                print("%%%%%%%%%%%%%", p[i : i + j], p[i - j : i])
-                p[i: i + j], p[i - j: i] = p[i - j: i], p[i: i + j]
+    while j <= m:
+        k = j//2
+        i = 1
+        while i <= m:
+            diff = p[i] - p[i + k]
+            if abs(diff) != k:
+                INVALID = True 
+                break 
+            if diff > 0:
+                p[i], p[i + k] = p[i + k], p[i]
                 count += 1
-            i += (j*2)
+            i += j
         if INVALID:
             break 
-        i = k + (j*2)
         j *= 2
     
     if INVALID:
         print(-1)
     else:
-        print(count, p)
+        print(count)
