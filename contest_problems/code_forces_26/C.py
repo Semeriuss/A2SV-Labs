6d2bbd292331aec76dfba69e430a55bfbefd3b09
Semere Habtu <se.semere.habtu@gmail.com>
2022-11-19 16:20:56 +0300

    simpler than it looked

--- contest_problems/code_forces_26/C.py
+++ contest_problems/code_forces_26/C.py
@@ -2,40 +2,17 @@ from itertools import accumulate
 for _ in range(int(input())):
     n = int(input())
     red = list(map(int, input().split()))
+    m1 = total = 0
+    for num in red:
+        total += num
+        m1 = max(m1, total)
+
+    
     m = int(input())
     blue = list(map(int, input().split()))
-
-    suffix_red = list(accumulate(reversed(red)))[::-1]
-    suffix_blue = list(accumulate(reversed(blue)))[::-1]
-
-    res = 0
-    i, j = 0, 0
-    total = 0
-    while i < n and j < m:
-        if red[i] >= blue[j]:
-            if red[i] >= 0 or suffix_red[i] >= suffix_blue[j]:
-                total += red[i]
-                i += 1
-            else:
-                total += blue[j]
-                j += 1
-        else:
-            if blue[j] >= 0 or suffix_blue[j] >= suffix_red[i]:
-                total += blue[j]
-                j += 1
-            else:
-                total += red[i]
-                i += 1
-        res = max(res, total)
+    m2 = total = 0
+    for num in blue:
+        total += num
+        m2 = max(total, m2)
     
-    while i < n:
-        total += red[i]
-        res = max(res, total)
-        i += 1
-    
-    while j < m:
-        total += blue[j]
-        res = max(res, total)
-        j += 1
-    
-    print(res)
+    print(m1 + m2)
