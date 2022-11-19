ed69a74a8deede1e712ad1f8ffe90e79190b2f60
Semere Habtu <se.semere.habtu@gmail.com>
2022-11-19 16:32:11 +0300

    cyclic components - loop sets

--- contest_problems/code_forces_26/D.py
+++ contest_problems/code_forces_26/D.py
@@ -1,54 +1,32 @@
-from collections import defaultdict
 n, m = map(int, input().split())
-parent = list(range(n + 1))
-children = [1]*(n + 1)
-
-def find(node):
-    if node != parent[node]:
-        parent[node] = parent[parent[node]]
-    return parent[node]
-
-def union(u, v):
-    root1, root2 = sorted([find(u), find(v)], key=lambda x : children[x])
-    if root1 != root2:
-        parent[root1] = root2
-        children[root2] += children[root1]
-
-graph = defaultdict(int)
+graph = [[] for _ in range(n + 1)]
 invalids = set()
 for _ in range(m):
     u, v = map(int, input().split())
-    union(u, v)
-    graph[u] += 1
-    graph[v] += 1
-    if graph[u] > 2:
-        invalids.add(u)
-        invalids.add(v)
-    if graph[v] > 2:
-        invalids.add(v)
-        invalids.add(u)
+    graph[u].append(v)
+    graph[v].append(u)
     
-for node, edgeCount in graph.items():
-    root = find(node)
-    if edgeCount == 1 or root in invalids:
-        invalids.add(node)
-        invalids.add(root)
+valids = set()
+for i in range(n + 1):
+    if len(graph[i]) == 2:
+        valids.add(i)
 
-
-for node in range(1, n + 1):
-    root = find(node)
-    if node in invalids:
-        invalids.add(root)
-    
-
-found = set()
 res = 0
-for node, edgeCount in graph.items():
-    root = find(node)
-    if edgeCount == 2 and root not in invalids and root not in found:
-        found.add(root)
-        res += 1
-
+while valids:
+    node = valids.pop()
+    left, right = graph[node]
+    while right in valids:
+        valids.remove(right)
+        next_left, next_right = graph[right]
+        if next_left in valids:
+            right = next_left
+        elif next_right in valids:
+            right = next_right 
+        else:
+            break 
+        if left == right:
+            res += 1
+            
 print(res)
 
 
