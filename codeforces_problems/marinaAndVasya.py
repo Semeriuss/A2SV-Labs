11a81a3a216a24b90a04d19cf235c1180e1d545b
Semere Habtu <se.semere.habtu@gmail.com>
2022-12-03 15:37:33 +0300

    EXHAUSTION

--- codeforces_problems/marinaAndVasya.py
+++ codeforces_problems/marinaAndVasya.py
@@ -13,38 +13,63 @@ elif t == 0:
 else:
     chars = {}
     for i in range(n):
-        chars[i] = 1 if s1[i] != s2[i] else 0
+        chars[i] = s1[i] if s1[i] == s2[i] else ""
 
-    
     res = ["-"] * n
-    char_count = 0
+    same = n - t 
 
-    for index, common in chars.items():
-        if char_count == n - t:
-            break
-        if common:
-            res[index] = s1[index]
-            char_count += 1
-    
-    if char_count != n - t:
-        print(-1)
-    else:
-        set1, set2 = set(list(s1)), set(list(s2))
-        common = set1.intersection(set2)
-        set1.difference_update(common)
-        set2.difference_update(common)
+    set1, set2 = set(list(s1)), set(list(s2))
+    common = set1.intersection(set2)
+    set1.difference_update(common)
+    set2.difference_update(common)
 
-        alphabet = set([chr(ord('a') + i) for i in range(26)])
+    alphabet = set([chr(ord('a') + i) for i in range(26)])
 
-        alphabet.difference_update(set1)
-        alphabet.difference_update(set2)
-        if not alphabet:
-            print(-1)
-        placeHolder = alphabet.pop()
+    if same == 0:
+        res = [""]*n
         for i in range(n):
-            if res[i] == "-":
-                res[i] = placeHolder
-    
-
-            
+            res[i] = set(filter(lambda x : x != s1[i] and x != s2[i], alphabet)).pop()
         print("".join(res))
+    else:
+        for i, c in chars.items():
+            if same == 0:
+                break 
+            elif c:
+                res[i] = c 
+                same -= 1
+        
+        if same == 0:
+            if alphabet:
+                char = alphabet.pop()
+                for i, c in enumerate(res):
+                    if c == '-':
+                        res[i] = set(filter(lambda x : x != s1[i] and x != s2[i], alphabet)).pop()
+                print("".join(res))
+            else:
+                print(-1)
+        else:
+            same1 = same2 = same
+            for i, c in enumerate(res):
+                if same1 == 0:
+                    break 
+                if c == '-':
+                    res[i] = s1[i]
+                    same1 -= 1
+            if same1 > 0:
+                print(-1)
+            else:
+                for i, c in enumerate(res):
+                    if same2 == 0:
+                        break 
+                    if c == '-':
+                        res[i] = s2[i]
+                        same2 -= 1
+                
+                if same2 > 0:
+                    print(-1)
+                else:
+                    for i in range(len(res)):
+                        if res[i] == '-':
+                            res[i] = res[i] = set(filter(lambda x : x != s1[i] and x != s2[i], alphabet)).pop()
+                    print("".join(res))
+        
