a8e9c0e8e4c46bc1c10dc3cb9a11f4bd03490391
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-01 23:59:44 +0300

    greedy, array

---
+++ squid_game_a2sv/day_1/4. earliestFullBloom.py
@@ -0,0 +1,10 @@
+class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        pairs = sorted([(gt, pt)
+                       for gt, pt in zip(growTime, plantTime)], reverse=True)
+        res = time = 0
+        for gTime, pTime in pairs:
+            time += pTime
+            res = max(res, time + gTime)
+
+        return res
