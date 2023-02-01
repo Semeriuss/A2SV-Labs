5717e2172e0abdbfa3340b928255cc4afce97ae0
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-01 15:28:59 +0300

    Array, Dynamic Programming

--- squid_game_a2sv/day_1/2. matrixBlockSum.py
+++ squid_game_a2sv/day_1/2. matrixBlockSum.py
@@ -2,19 +2,18 @@ from ast import List
 
 
 class Solution:
-    # O(N*M * N^2) time, O(N*M) auxiliary space
+    # O(N*M) time and O(N*M) space
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
         N, M = len(mat), len(mat[0])
         answer = [[0] * M for _ in range(N)]
 
-        def getSum(row_range, col_range):
-            ri, rf = row_range
-            ci, cf = col_range
-            total = 0
-            for r in range(ri, rf + 1):
-                for c in range(ci, cf + 1):
-                    total += mat[r][c]
-            return total
+        dp = mat[:][:]
+        for i in range(N):
+            for j in range(M):
+                dp[i][j] += (dp[i - 1][j] if (i - 1) >= 0 else 0) + \
+                            (dp[i][j - 1] if (j - 1) >= 0 else 0) - \
+                            (dp[i - 1][j - 1] if (i - 1)
+                             >= 0 and (j - 1) >= 0 else 0)
 
         for i in range(N):
             ri = max(0, i - k)
@@ -22,6 +21,12 @@ class Solution:
             for j in range(M):
                 ci = max(0, j - k)
                 cf = min(M - 1, j + k)
-                answer[i][j] += getSum((ri, rf), (ci, cf))
+                answer[i][j] = dp[rf][cf]
+                if ri > 0:
+                    answer[i][j] -= dp[ri - 1][cf]
+                if ci > 0:
+                    answer[i][j] -= dp[rf][ci - 1]
+                if ri > 0 and ci > 0:
+                    answer[i][j] += dp[ri - 1][ci - 1]
 
         return answer
