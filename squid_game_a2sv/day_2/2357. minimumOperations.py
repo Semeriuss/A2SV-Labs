25cc1612db520cef0df63306120ba2fec579f237
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-03 00:11:48 +0300

    Improved algorithm

--- squid_game_a2sv/day_2/2357. minimumOperations.py
+++ squid_game_a2sv/day_2/2357. minimumOperations.py
@@ -1,25 +1,11 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
-    # O(N^2) time, O(1) space
+    # O(N) time, O(1) space
     def minimumOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        zero_count = nums.count(0)
-        positive_count = n - zero_count
-
-        res = 0
-        while positive_count > 0:
-            res += 1
-            x = 1000
-            for num in nums:
-                if num > 0 and num < x:
-                    x = num
-
-            for i in range(len(nums)):
-                if nums[i] > 0:
-                    nums[i] -= x
-                    if nums[i] == 0:
-                        positive_count -= 1
-
-        return res
+        freq = Counter(nums)
+        ops = len(freq)
+        ops -= 1 if freq[0] > 0 else 0
+        return ops
