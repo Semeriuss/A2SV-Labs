34fbbd8e33f738a8d0b78ae1ccb4ef80984ce3ee
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-02 18:13:00 +0300

    Add import statement

--- squid_game_a2sv/day_1/4. earliestFullBloom.py
+++ squid_game_a2sv/day_1/4. earliestFullBloom.py
@@ -1,4 +1,8 @@
+from typing import List
+
+
 class Solution:
+    # O(N) time | O(1) space
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
         pairs = sorted([(gt, pt)
                        for gt, pt in zip(growTime, plantTime)], reverse=True)
