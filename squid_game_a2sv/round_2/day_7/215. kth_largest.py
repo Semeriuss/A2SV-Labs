0065ccb96939e6cd4218c799a77fa4aa175f68c7
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-07 18:38:08 +0300

    Sorting

---
+++ squid_game_a2sv/round_2/day_7/215. kth_largest.py
@@ -0,0 +1,31 @@
+from random import randint
+from typing import List
+
+
+class Solution:
+    # O(N) time and O(1) space
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        def quickSelect(l, r, k):
+            def partition(left, right, pindex):
+                pivot = nums[pindex]
+                nums[right], nums[pindex] = nums[pindex], nums[right]
+                pindex = left
+                for i in range(left, right):
+                    if nums[i] <= pivot:
+                        nums[pindex], nums[i] = nums[i], nums[pindex]
+                        pindex += 1
+                nums[pindex], nums[right] = nums[right], nums[pindex]
+                return pindex
+            if l == r:
+                return nums[l]
+            pivotIndex = randint(l, r)
+            p = partition(l, r, pivotIndex)
+            if p > k:
+                return quickSelect(l, p - 1, k)
+            elif p < k:
+                return quickSelect(p + 1, r, k)
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1, len(nums) - k)
