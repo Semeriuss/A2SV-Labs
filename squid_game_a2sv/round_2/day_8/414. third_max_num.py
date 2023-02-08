8d2e8269cd38ace540500a73da1e85d327e0a371
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-08 17:32:47 +0300

    Heap, Hash Set

---
+++ squid_game_a2sv/round_2/day_8/414. third_max_num.py
@@ -0,0 +1,20 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    # O(N) time and space
+    def thirdMax(self, nums: List[int]) -> int:
+        heap = []
+        maxNum = 0
+        seen = set()
+        for num in nums:
+            if num in seen:
+                continue
+            seen.add(num)
+            maxNum = max(num, maxNum)
+            if len(heap) == 3:
+                heapq.heappushpop(heap, num)
+            else:
+                heapq.heappush(heap, num)
+        return heapq.heappop(heap) if len(heap) == 3 else maxNum
