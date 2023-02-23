60ed2a7bbf0391320fa83941de2cfbda0f4b0b98
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-23 22:50:07 +0300

    DP

---
+++ squid_game_a2sv/round_4/day_23/diffWaysToCompute.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    @lru_cache()
+    def diffWaysToCompute(self, expr: str) -> List[int]:
+        if expr.isdigit():
+            return [int(expr)]
+        res = []
+        for i, c in enumerate(expr):
+            if c in '*+-':
+                leftWays = self.diffWaysToCompute(expr[:i])
+                rightWays = self.diffWaysToCompute(expr[i+1:])
+                for left in leftWays:
+                    for right in rightWays:
+                        if c == '*':
+                            res.append(left * right)
+                        elif c == '+':
+                            res.append(left + right)
+                        else:
+                            res.append(left - right)
+        return res
