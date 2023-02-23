e86140759043458aa571708027b0359b1f0a2df3
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-23 22:42:11 +0300

    DP

---
+++ squid_game_a2sv/round_4/day_23/burstBalloons.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    # O(N^3) time and O(N^2) space
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 2, n):
+                dp[i][j] = max([dp[i][k] + nums[i] * nums[k] *
+                               nums[j] + dp[k][j] for k in range(i + 1, j)])
+        return dp[0][n - 1]
