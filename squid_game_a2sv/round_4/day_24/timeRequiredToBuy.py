7bfe0bf1baa372e8dbdfa300f6a85000bbc5fc0f
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-24 22:23:11 +0300

    Array

--- squid_game_a2sv/round_4/day_24/timeRequiredToBuy.py
+++ squid_game_a2sv/round_4/day_24/timeRequiredToBuy.py
@@ -2,23 +2,14 @@ from typing import List
 
 
 class Solution:
+    # O(N) time and O(k) space
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        # O(max(tickets)) time | O(1) space
-        time = 0
-        i = 0
-        while tickets[k] != 0:
-            if i >= len(tickets):
-                i = 0
+        n = len(tickets)
+        totalArea = tickets[k] * n
+        for i in range(n):
+            totalArea -= (tickets[k] - min(tickets[k], tickets[i]))
 
-            if tickets[i] == 0 and i == k:
-                break
-
-            if tickets[i] > 0:
-                tickets[i] -= 1
-                time += 1
-                i += 1
-
-            elif tickets[i] == 0:
-                i += 1
-
-        return time
+        for i in range(k + 1, n):
+            if tickets[i] >= tickets[k]:
+                totalArea -= 1
+        return totalArea
