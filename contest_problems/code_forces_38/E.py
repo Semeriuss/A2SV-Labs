1f6130b50e79bafc7783e721fd31f5498cbb7e9b
Semere Habtu <se.semere.habtu@gmail.com>
2023-02-25 11:34:37 +0300

    Equalize the Array

--- contest_problems/code_forces_38/E.py
+++ contest_problems/code_forces_38/E.py
@@ -1,28 +1,16 @@
 from collections import Counter
-from heapq import heappush, heappop
 for _ in range(int(input())):
     n = int(input())
-    a = list(map(int, input().split()))
-    freq = Counter(a)
-    k = max(freq.values())
-    rep = [0] * (k + 1)
-    for num in freq:
-        rep[freq[num]] += 1
-    heap = []
-    for i in range(1, k + 1):
-        if (rep[i] * i) != 0:
-            heappush(heap, (-(rep[i] * i), -i, -rep[i]))
+    freq = Counter(map(int, input().split()))
+    freqGrp = Counter(freq.values())
 
-    _, count, res = heappop(heap)
-    res, count = -res, -count
-    ans = 0
-    ret = [float('-inf')] * (k + 1)
-    while heap:
-        _, freq, cur = heappop(heap)
-        cur, freq = -cur, -freq
-        if freq > count:
-            ans += (cur * (freq - count))
-        else:
-            ans += (cur * freq)
-
-    print(ans)
+    res = n
+    for x in freqGrp:
+        temp = 0
+        for y in freqGrp:
+            if x > y:
+                temp += y * freqGrp[y]
+            else:
+                temp += (y - x) * freqGrp[y]
+        res = min(res, temp)
+    print(res)
